8da273a67ce75bc0dbfb6ab729f3d0076b38b5d2
Rico Suter <mail@rsuter.com>
2015-09-08 18:58:04 +0200

    Added NSwag.Annotations

---
+++ src/NSwag.Annotations/SwaggerResultType.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SwaggerResultTypeAttribute.cs" company="NSwag">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/NSwag/NSwag/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace NSwag.Annotations
+{
+    /// <summary>Specifies the result type of a web service method to correctly generate a Swagger definition.</summary>
+    public class SwaggerResultTypeAttribute : Attribute
+    {
+        /// <summary>Initializes a new instance of the <see cref="SwaggerResultTypeAttribute"/> class.</summary>
+        /// <param name="resultType">The operation result type.</param>
+        public SwaggerResultTypeAttribute(Type resultType)
+        {
+            ResultType = resultType; // TODO: Check for this attribute on WebAPI methods
+        }
+
+        /// <summary>Gets or sets the result type.</summary>
+        public Type ResultType { get; set; }
+    }
+}
