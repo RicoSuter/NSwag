61b7920b10d9abd9b1863063aa182fea35cd9427
Rico Suter <mail@rsuter.com>
2015-10-19 19:26:05 +0200

    Added descriptions

--- src/NSwag.CodeGeneration/Properties/AssemblyInfo.cs
+++ src/NSwag.CodeGeneration/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("NSwag.CodeGeneration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NSwag: The Swagger API toolchain for .NET")]
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwag.CodeGeneration")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2015")]
--- src/NSwag.Core/Properties/AssemblyInfo.cs
+++ src/NSwag.Core/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("NSwag")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NSwag: The Swagger API toolchain for .NET")]
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwag")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2015")]
