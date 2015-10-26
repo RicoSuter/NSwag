76a60877eca87f4c5cb72ae6d0e87b3ece7eb70b
Rico Suter <mail@rsuter.com>
2015-10-26 18:14:39 +0100

    Now storing window state to app settings

--- src/NSwagStudio/Views/MainWindow.xaml.cs
+++ src/NSwagStudio/Views/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Documents;
+using MyToolkit.Storage;
 using MyToolkit.Utilities;
 
 namespace NSwagStudio.Views
@@ -11,6 +13,7 @@ namespace NSwagStudio.Views
         {
             InitializeComponent();
             CheckForApplicationUpdate();
+            LoadWindowState();
         }
 
         private async void CheckForApplicationUpdate()
@@ -21,6 +24,27 @@ namespace NSwagStudio.Views
                 "http://rsuter.com/Projects/NSwagStudio/updates.php");
 
             await updater.CheckForUpdate(this);
+        }
+
+        private void LoadWindowState()
+        {
+            Width = ApplicationSettings.GetSetting("WindowWidth", Width);
+            Height = ApplicationSettings.GetSetting("WindowHeight", Height);
+            Left = ApplicationSettings.GetSetting("WindowLeft", Left);
+            Top = ApplicationSettings.GetSetting("WindowTop", Top);
+            WindowState = ApplicationSettings.GetSetting("WindowState", WindowState);
+
+            if (Left == double.NaN)
+                WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            ApplicationSettings.SetSetting("WindowWidth", Width);
+            ApplicationSettings.SetSetting("WindowHeight", Height);
+            ApplicationSettings.SetSetting("WindowLeft", Left);
+            ApplicationSettings.SetSetting("WindowTop", Top);
+            ApplicationSettings.SetSetting("WindowState", WindowState);
         }
 
         private void OnOpenHyperlink(object sender, RoutedEventArgs e)
