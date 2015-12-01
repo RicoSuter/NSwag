d531de871994541a7c10cc24a7a7ca2c05766713
Rico Suter <mail@rsuter.com>
2015-12-01 17:15:17 +0100

    Added xml doc

--- src/NSwag.Core/HttpUtilities.cs
+++ src/NSwag.Core/HttpUtilities.cs
@@ -8,8 +8,12 @@
 
 namespace NSwag
 {
+    /// <summary>Contains HTTP utilities.</summary>
     public static class HttpUtilities
     {
+        /// <summary>Checks whether the given HTTP status code indicates success.</summary>
+        /// <param name="statusCode">The HTTP status code.</param>
+        /// <returns>true if success.</returns>
         public static bool IsSuccessStatusCode(string statusCode)
         {
             return statusCode.Length == 3 && statusCode.StartsWith("2");
