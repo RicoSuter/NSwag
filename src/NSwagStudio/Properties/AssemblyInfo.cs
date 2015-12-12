c1d9e44f74917ca207247e19af7b9ed56a61d634
Rico Suter <mail@rsuter.com>
2015-12-12 02:52:41 +0100

    Updated NJsonSchema, release v1.12

--- src/NSwagStudio/Properties/AssemblyInfo.cs
+++ src/NSwagStudio/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwagStudio")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2015")]
-[assembly: AssemblyVersion("1.11.*")]
+[assembly: AssemblyVersion("1.12.*")]
