18cfadfece7c194ae0b25362990a7640f21e7247
Rico Suter <mail@rsuter.com>
2016-01-13 18:57:11 +0100

    Improved templates for date, v1.22

--- src/NSwag/Properties/AssemblyInfo.cs
+++ src/NSwag/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwag.Console")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2015")]
-[assembly: AssemblyVersion("1.21.*")]
+[assembly: AssemblyVersion("1.22.*")]
