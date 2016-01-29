b7e946ab647f46133d7b08543bff447a594a777d
Rico Suter <mail@rsuter.com>
2016-01-29 16:35:00 +0100

    Release v1.27

--- src/NSwag.Annotations/Properties/AssemblyInfo.cs
+++ src/NSwag.Annotations/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwag.Annotations")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2015")]
-[assembly: AssemblyVersion("1.26.*")]
+[assembly: AssemblyVersion("1.27.*")]
