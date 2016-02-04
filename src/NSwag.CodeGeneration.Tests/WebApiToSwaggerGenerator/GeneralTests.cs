34a924b1c66133183bba6a8fbee204f48bcca52f
Rico Suter <mail@rsuter.com>
2016-02-04 20:46:54 +0100

    Added type load exception

---
+++ src/NSwag.CodeGeneration.Tests/WebApiToSwaggerGenerator/GeneralTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSwag.CodeGeneration.SwaggerGenerators.WebApi;
+
+namespace NSwag.CodeGeneration.Tests.WebApiToSwaggerGenerator
+{
+    [TestClass]
+    public class GeneralTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(TypeLoadException))]
+        public void When_controller_type_is_not_found_then_type_load_exception_is_thrown()
+        {
+            //// Arrange
+            var settings = new WebApiAssemblyToSwaggerGeneratorSettings
+            {
+                AssemblyPath = @"./NSwag.CodeGeneration.Tests.dll",
+                DefaultUrlTemplate = "api/{controller}/{action}/{id}"
+            };
+
+            var generator = new WebApiAssemblyToSwaggerGenerator(settings);
+
+            //// Act
+            var swaggerService = generator.GenerateForController("NonExistingClass"); // Should throw exception
+
+            //// Assert
+        }
+    }
+}
