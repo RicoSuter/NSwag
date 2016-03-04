46c4f7fc85745b396b087fab5363acd7f44891da
Rico Suter <mail@rsuter.com>
2016-03-04 23:25:47 +0100

    Added unit test

--- src/NSwag.CodeGeneration.Tests/WebApiToSwaggerGenerator/Attributes/ComplexParametersTests.cs
+++ src/NSwag.CodeGeneration.Tests/WebApiToSwaggerGenerator/Attributes/ComplexParametersTests.cs
@@ -11,6 +11,8 @@ namespace NSwag.CodeGeneration.Tests.WebApiToSwaggerGenerator.Attributes
         public class MyParameter
         {
             public string Foo { get; set; }
+
+            public string Bar { get; set; }
         }
 
         public class TestController : ApiController
@@ -43,6 +45,7 @@ namespace NSwag.CodeGeneration.Tests.WebApiToSwaggerGenerator.Attributes
 
             //// Assert
             Assert.AreEqual(SwaggerParameterKind.Body, operation.Parameters[0].Kind);
+            Assert.AreEqual("data", operation.Parameters[0].Name);
         }
 
         [TestMethod]
@@ -59,6 +62,22 @@ namespace NSwag.CodeGeneration.Tests.WebApiToSwaggerGenerator.Attributes
             Assert.AreEqual(SwaggerParameterKind.Query, operation.Parameters[0].Kind);
         }
 
+        [TestMethod]
+        public void When_parameter_is_complex_and_has_FromUri_then_complex_object_properties_are_added()
+        {
+            //// Arrange
+            var generator = new SwaggerGenerators.WebApi.WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+
+            //// Act
+            var service = generator.GenerateForController<TestController>();
+            var operation = service.Operations.Single(o => o.Operation.OperationId == "WithFromUriAttribute").Operation;
+
+            //// Assert
+            Assert.AreEqual(SwaggerParameterKind.Query, operation.Parameters[0].Kind);
+            Assert.AreEqual(SwaggerParameterKind.Query, operation.Parameters[1].Kind);
+            Assert.AreEqual("Foo", operation.Parameters[0].Name);
+            Assert.AreEqual("Bar", operation.Parameters[1].Name);
+        }
 
         [TestMethod]
         public void When_parameter_is_complex_and_has_FromBody_then_it_is_a_body_parameter()
@@ -72,6 +91,7 @@ namespace NSwag.CodeGeneration.Tests.WebApiToSwaggerGenerator.Attributes
 
             //// Assert
             Assert.AreEqual(SwaggerParameterKind.Body, operation.Parameters[0].Kind);
+            Assert.AreEqual("data", operation.Parameters[0].Name);
         }
     }
 }
