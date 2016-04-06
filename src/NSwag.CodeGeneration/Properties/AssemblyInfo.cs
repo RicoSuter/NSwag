e2c22f4a4406a571e8de67e6388549de742f657a
Rico Suter <mail@rsuter.com>
2016-04-06 18:22:36 +0200

    Release v2.4

--- src/NSwag.CodeGeneration/Properties/AssemblyInfo.cs
+++ src/NSwag.CodeGeneration/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwag.CodeGeneration")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2015")]
-[assembly: AssemblyVersion("2.3.*")]
+[assembly: AssemblyVersion("2.4.*")]
