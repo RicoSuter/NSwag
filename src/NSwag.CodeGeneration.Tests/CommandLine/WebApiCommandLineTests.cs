16dea6b33495e4778904b49fc5f99d2440be4e89
Rico Suter <mail@rsuter.com>
2016-04-29 17:33:47 +0200

    Fixed command line, updated packages, added unit test, v2.12

---
+++ src/NSwag.CodeGeneration.Tests/CommandLine/WebApiCommandLineTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NSwag.CodeGeneration.Tests.CommandLine
+{
+    [TestClass]
+    public class WebApiCommandLineTests
+    {
+        [TestMethod]
+        public void When_webapi2swagger_is_called_then_file_is_created()
+        {
+            //// Arrange
+            if (File.Exists("MyWebService.json"))
+                File.Delete("MyWebService.json");
+
+            var command = "webapi2swagger " +
+                          "/assembly:" + Path.GetFullPath("../../../NSwag.Demo.Web/bin/NSwag.Demo.Web.dll") + " " +
+                          "/controller:NSwag.Demo.Web.Controllers.PersonsController " +
+                          "/output:MyWebService.json";
+
+            //// Act
+            var process = Process.Start(new ProcessStartInfo
+            {
+                FileName = Path.GetFullPath("../../../NSwag/bin/" + (Directory.GetCurrentDirectory().Contains("bin\\Release") ? "Release" : "Debug") + "/NSwag.exe"),
+                Arguments = command,
+            });
+
+            if (!process.WaitForExit(5000))
+                process.Kill();
+
+            var json = File.ReadAllText("MyWebService.json");
+
+            //// Assert
+            Assert.IsTrue(!string.IsNullOrEmpty(json));
+        }
+    }
+}
