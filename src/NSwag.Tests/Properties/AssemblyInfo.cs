c5f16c96c735a9af56453f5968e028de7b4bfde9
Rico Suter <mail@rsuter.com>
2016-05-18 22:32:59 +0200

    Fixed method loading, v2.26

--- src/NSwag.Tests/Properties/AssemblyInfo.cs
+++ src/NSwag.Tests/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.25.*")]
+// [assembly: AssemblyVersion("2.26.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
