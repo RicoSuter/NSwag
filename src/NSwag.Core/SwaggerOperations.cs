e0d351a54bb6bb607edb3a218044f82504b11635
Rico Suter <mail@rsuter.com>
2016-05-24 23:23:10 +0200

    Fixed SwaggerOperations serialization (lower case keys), v2.36

--- src/NSwag.Core/SwaggerOperations.cs
+++ src/NSwag.Core/SwaggerOperations.cs
@@ -6,6 +6,7 @@
 // <author>Rico Suter, mail@rsuter.com</author>
 //-----------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using NSwag.Collections;
@@ -13,6 +14,7 @@ using NSwag.Collections;
 namespace NSwag
 {
     /// <summary>A Swagger path.</summary>
+    [JsonConverter(typeof(SwaggerOperationsJsonConverter))]
     public class SwaggerOperations : ObservableDictionary<SwaggerOperationMethod, SwaggerOperation>
     {
         /// <summary>Initializes a new instance of the <see cref="SwaggerOperations"/> class.</summary>
@@ -21,7 +23,7 @@ namespace NSwag
             CollectionChanged += (sender, args) =>
             {
                 foreach (var operation in Values)
-                    operation.Parent = this; 
+                    operation.Parent = this;
             };
         }
 
@@ -32,5 +34,42 @@ namespace NSwag
         /// <summary>Gets or sets the parameters.</summary>
         [JsonProperty(PropertyName = "parameters", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public List<SwaggerParameter> Parameters { get; set; }
+
+        // Needed to convert dictionary keys to lower case
+        internal class SwaggerOperationsJsonConverter : JsonConverter
+        {
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                var operations = (SwaggerOperations)value;
+                writer.WriteStartObject();
+                foreach (var pair in operations)
+                {
+                    writer.WritePropertyName(pair.Key.ToString().ToLower());
+                    serializer.Serialize(writer, pair.Value);
+                }
+                writer.WriteEndObject();
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                if (reader.TokenType == JsonToken.Null)
+                    return null;
+
+                var operations = new SwaggerOperations();
+                while (reader.Read() && reader.TokenType == JsonToken.PropertyName)
+                {
+                    var key = (SwaggerOperationMethod)Enum.Parse(typeof(SwaggerOperationMethod), reader.Value.ToString(), true);
+                    reader.Read();
+                    var value = (SwaggerOperation)serializer.Deserialize(reader, typeof(SwaggerOperation));
+                    operations.Add(key, value);
+                }
+                return operations;
+            }
+
+            public override bool CanConvert(Type objectType)
+            {
+                return objectType == typeof(SwaggerOperations);
+            }
+        }
     }
 }
