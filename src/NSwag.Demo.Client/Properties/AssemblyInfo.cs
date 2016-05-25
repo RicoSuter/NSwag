17d8e4f9b748042e964b8d35c036bff87e592d17
Rico Suter <mail@rsuter.com>
2016-05-26 00:56:28 +0200

    Updated NJsonSchema, v2.37

--- src/NSwag.Demo.Client/Properties/AssemblyInfo.cs
+++ src/NSwag.Demo.Client/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.36.*")]
+// [assembly: AssemblyVersion("2.37.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
