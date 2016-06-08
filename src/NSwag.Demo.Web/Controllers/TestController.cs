0cd9c522b3a984762496d704af7a955d31e08cd0
Rico Suter <mail@rsuter.com>
2016-06-08 09:09:57 +0200

    v3.0

---
+++ src/NSwag.Demo.Web/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace NSwag.Demo.Web.Controllers
+{
+    public class TestController : ApiController
+    {
+        public object Test()
+        {
+            return null; 
+        }
+    }
+}
