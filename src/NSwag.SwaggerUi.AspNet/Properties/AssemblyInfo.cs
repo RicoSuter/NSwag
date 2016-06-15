1525c76f376d52a3ca579e2f8f47809f5f21124d
Rico Suter <mail@rsuter.com>
2016-06-15 22:39:40 +0200

    Added description

--- src/NSwag.SwaggerUi.AspNet/Properties/AssemblyInfo.cs
+++ src/NSwag.SwaggerUi.AspNet/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("NSwag.SwaggerUi.AspNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NSwag: The Swagger API toolchain for .NET and TypeScript")]
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwag.SwaggerUi.AspNet")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2015")]
