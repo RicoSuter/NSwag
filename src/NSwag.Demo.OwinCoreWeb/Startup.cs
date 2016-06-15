610728b8d3ba73cd7d4e8dc400f31aa21be1682d
Rico Suter <mail@rsuter.com>
2016-06-15 22:31:35 +0200

    Updated NJsonSchema, added NSwag.SwaggerUi.AspNet* projects, added SwaggerTagsAttribute

---
+++ src/NSwag.Demo.OwinCoreWeb/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSwag.Demo.OwinCoreWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            //app.UseSwaggerUi(typeof(Startup).GetTypeInfo().Assembly/*, new WebApiToSwaggerGeneratorSettings()*/);
+        }
+    }
+}
---
+++ src/NSwag.SwaggerUi.AspNet/RedirectMiddleware.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RedirectMiddleware.cs" company="NSwag">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/NSwag/NSwag/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace NSwag.SwaggerUi.AspNet
+{
+    internal class RedirectMiddleware : OwinMiddleware
+    {
+        private readonly string _fromPath;
+        private readonly string _toPath;
+
+        public RedirectMiddleware(OwinMiddleware next, string fromPath, string toPath) 
+            : base(next)
+        {
+            _fromPath = fromPath;
+            _toPath = toPath;
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            var url = context.Request.Uri;
+            if (url.PathAndQuery.Trim('/') == _fromPath.Trim('/'))
+            {
+                context.Response.StatusCode = 301;
+                context.Response.Headers.Set("Location", _toPath);
+            }
+            else
+                await Next.Invoke(context);
+        }
+    }
+}
