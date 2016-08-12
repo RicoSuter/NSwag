73816f78708d8b86dedef45a3d41ca25d179de6d
Rico Suter <mail@rsuter.com>
2016-08-12 11:24:31 +0200

    Improved AssemblyConfigurationFileTransformer

--- src/NSwag.AssemblyLoader/Infrastructure/AssemblyConfigurationFileTransformer.cs
+++ src/NSwag.AssemblyLoader/Infrastructure/AssemblyConfigurationFileTransformer.cs
@@ -8,7 +8,6 @@
 
 using System;
 using System.IO;
-using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -16,27 +15,58 @@ namespace NSwag.CodeGeneration.Infrastructure
 {
     internal class AssemblyConfigurationFileTransformer : IDisposable
     {
+        private const string EmptyConfig =
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <runtime>
+    <assemblyBinding xmlns=""urn:schemas-microsoft-com:asm.v1"">
+    </assemblyBinding>
+  </runtime>
+</configuration>";
+
+        private readonly string JsonNetAssemblyBinding =
+@"<dependentAssembly>
+  <assemblyIdentity name=""Newtonsoft.Json"" publicKeyToken=""30ad4fe6b2a6aeed"" culture=""neutral"" />
+  <bindingRedirect oldVersion=""0.0.0.0-9999.0.0.0"" newVersion=""9.0.0.0"" />
+</dependentAssembly>";
+
+        private readonly string NSwagCoreAssemblyBinding =
+@"<dependentAssembly>
+  <assemblyIdentity name=""NSwag.Core"" publicKeyToken=""c2d88086e098d109"" culture=""neutral"" />
+  <bindingRedirect oldVersion=""0.0.0.0-9999.0.0.0"" newVersion=""" + SwaggerService.ToolchainVersion + @""" />
+</dependentAssembly>";
+
         private string _transformedConfigurationPath = null;
 
         public string GetConfigurationPath(string assemblyDirectory)
         {
-            var configPath = GetOriginalConfigurationPath(assemblyDirectory);
-            if (configPath != null)
-            {
-                var content = File.ReadAllText(configPath, Encoding.UTF8);
-                configPath = configPath + ".nswagtemp";
+            var configPath = TryFindConfigurationPath(assemblyDirectory);
+            var content = configPath != null ? File.ReadAllText(configPath, Encoding.UTF8) : EmptyConfig;
 
-                // Transform Newtonsoft.Json binding redirect so that all code uses the same JSON classes
-                content = Regex.Replace(content, "<assemblyIdentity name=\"Newtonsoft.Json\"((.|\n|\r)*?)</dependentAssembly>",
+            content = UpdateOrAddBindingRedirect(content, "Newtonsoft.Json", "9.0.0.0", JsonNetAssemblyBinding);
+            content = UpdateOrAddBindingRedirect(content, "NSwag.Core", SwaggerService.ToolchainVersion, NSwagCoreAssemblyBinding);
+
+            _transformedConfigurationPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".nswagtemp");
+            File.WriteAllText(_transformedConfigurationPath, content, Encoding.UTF8);
+            return _transformedConfigurationPath;
+        }
+
+        private string UpdateOrAddBindingRedirect(string content, string name, string newVersion, string assemblyBinding)
+        {
+            if (content.Contains(@"assemblyIdentity name=""" + name + @""""))
+            {
+                content = Regex.Replace(content, "<assemblyIdentity name=\"" + name + "\"((.|\n|\r)*?)</dependentAssembly>",
                     match => Regex.Replace(match.Value, "oldVersion=\"(.*?)\"", "oldVersion=\"0.0.0.0-9999.0.0.0\""),
                     RegexOptions.Singleline);
 
-                File.WriteAllText(configPath, content, Encoding.UTF8);
-                _transformedConfigurationPath = configPath;
-                return configPath;
-            }
+                content = Regex.Replace(content, "<assemblyIdentity name=\"" + name + "\"((.|\n|\r)*?)</dependentAssembly>",
+                    match => Regex.Replace(match.Value, "newVersion=\"(.*?)\"", "newVersion=\"" + newVersion + "\""),
+                    RegexOptions.Singleline);
 
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "App.config");
+                return content;
+            }
+            else
+                return content.Replace(" </assemblyBinding>", assemblyBinding + "</assemblyBinding>");
         }
 
         public void Dispose()
@@ -44,11 +74,11 @@ namespace NSwag.CodeGeneration.Infrastructure
             if (_transformedConfigurationPath != null)
             {
                 File.Delete(_transformedConfigurationPath);
-                _transformedConfigurationPath = null; 
+                _transformedConfigurationPath = null;
             }
         }
 
-        private string GetOriginalConfigurationPath(string assemblyDirectory)
+        private string TryFindConfigurationPath(string assemblyDirectory)
         {
             var config = Path.Combine(assemblyDirectory, "App.config");
             if (File.Exists(config))
