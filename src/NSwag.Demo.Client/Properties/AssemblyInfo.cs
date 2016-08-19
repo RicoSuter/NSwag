b4b7cb401faf6330e0d4e1a49b9a35f54f9fdbd7
Rico Suter <mail@rsuter.com>
2016-08-19 17:31:58 +0200

    v5.1

--- src/NSwag.Demo.Client/Properties/AssemblyInfo.cs
+++ src/NSwag.Demo.Client/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("5.0.*")]
+// [assembly: AssemblyVersion("5.1.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
