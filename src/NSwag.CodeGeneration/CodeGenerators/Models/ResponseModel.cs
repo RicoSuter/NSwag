0d284ec924e4966f91a40ccc1b86fea5862b1eda
PawelZ <pawel.zawadzki@britishcouncil.pl>
2016-08-23 10:02:15 +0200

    Better merge

--- src/NSwag.CodeGeneration/CodeGenerators/Models/ResponseModel.cs
+++ src/NSwag.CodeGeneration/CodeGenerators/Models/ResponseModel.cs
@@ -6,7 +6,6 @@
 // <author>Rico Suter, mail@rsuter.com</author>
 //-----------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using NJsonSchema;
