aa678a098a86e3ff8b9a60754f081d3e4c4ccfaa
Rico Suter <mail@rsuter.com>
2016-09-06 01:00:21 +0200

    Added controller

---
+++ src/NSwag.Integration.WebAPI/Controllers/GeoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using NSwag.Integration.WebAPI.Models;
+
+namespace NSwag.Integration.WebAPI.Controllers
+{
+    public class GeoController : ApiController
+    {
+        public void FromBodyTest([FromBody] GeoPoint location)
+        {
+
+        }
+
+        public void FromUriTest([FromUri] GeoPoint location)
+        {
+
+        }
+    }
+}
---
+++ src/NSwag.Integration.WebAPI/Models/GeoPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace NSwag.Integration.WebAPI.Models
+{
+    public class GeoPoint
+    {
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
