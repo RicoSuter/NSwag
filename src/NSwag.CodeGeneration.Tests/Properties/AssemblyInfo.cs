b80f64d28b676dfe525d74a014cbe2969216f34a
Rico Suter <mail@rsuter.com>
2016-10-11 14:50:23 +0200

    v6.7

--- src/NSwag.CodeGeneration.Tests/Properties/AssemblyInfo.cs
+++ src/NSwag.CodeGeneration.Tests/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("6.6.*")]
+// [assembly: AssemblyVersion("6.7.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
