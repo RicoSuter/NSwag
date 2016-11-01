d6f161e9d05d7416fe8c2a0caa1c2b0070bd3b34
Rico Suter <mail@rsuter.com>
2016-11-01 10:56:08 +0100

    Removed .bat

--- src/NSwag.AspNetCore/build.bat
+++
@@ -1,5 +0,0 @@
-rmdir Output /Q /S nonemptydir
-
-del NSwag.AssemblyLoaderCore/project.lock.json
-dotnet restore NSwag.AssemblyLoaderCore/ --no-cache
-dotnet pack --output Output --configuration Release
