5008616dad5d0175d5fb82df073d69d3a957d348
Rico Suter <mail@rsuter.com>
2016-11-04 00:14:54 +0100

    Improved NPM build

--- build/05_Npm_Build.bat
+++ build/05_Npm_Build.bat
@@ -5,6 +5,8 @@ dotnet build
 
 dotnet publish -c release
 
-xcopy "bin\release\netcoreapp1.0\publish" "..\NSwag.ConsoleCore.Npm\bin\binaries"
+rmdir "..\NSwag.ConsoleCore.Npm\bin\binaries" /Q /S nonemptydir
+mkdir "..\NSwag.ConsoleCore.Npm\bin\binaries"
+xcopy "bin\release\netcoreapp1.0\publish" "..\NSwag.ConsoleCore.Npm\bin\binaries" /E /I /y
 
 cd ../../build
