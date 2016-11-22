7db4e2f21c55b7d77c669737d732c6c177427abe
Rico Suter <mail@rsuter.com>
2016-11-22 16:06:26 +0100

    v7.5, closes #439

--- src/NSwag.Core/Properties/AssemblyInfo.cs
+++ src/NSwag.Core/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwag")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("7.4.*")]
+[assembly: AssemblyVersion("7.5.*")]
