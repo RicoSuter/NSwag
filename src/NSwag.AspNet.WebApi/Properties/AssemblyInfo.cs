f2a4c85e54c1ee24c69af4aab5556e79e422640f
Rico Suter <mail@rsuter.com>
2016-11-22 15:21:31 +0100

    v7.4

--- src/NSwag.AspNet.WebApi/Properties/AssemblyInfo.cs
+++ src/NSwag.AspNet.WebApi/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwag.AspNet.WebApi")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("7.3.*")]
+[assembly: AssemblyVersion("7.4.*")]
