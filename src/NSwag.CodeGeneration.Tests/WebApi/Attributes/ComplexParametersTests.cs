81114ed619cea2819e36a2b726059ccc51cedb3a
Rico Suter <mail@rsuter.com>
2017-01-10 16:20:50 +0100

    Add support for FromRouteAttribute in complex parameters, closes #513

--- src/NSwag.CodeGeneration.Tests/WebApi/Attributes/ComplexParametersTests.cs
+++ src/NSwag.CodeGeneration.Tests/WebApi/Attributes/ComplexParametersTests.cs
@@ -1,8 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using NJsonSchema;
 using NSwag.CodeGeneration.SwaggerGenerators.WebApi;
 
 namespace NSwag.CodeGeneration.Tests.WebApi.Attributes
@@ -98,5 +102,57 @@ namespace NSwag.CodeGeneration.Tests.WebApi.Attributes
             Assert.AreEqual(SwaggerParameterKind.Body, operation.ActualParameters[0].Kind);
             Assert.AreEqual("data", operation.ActualParameters[0].Name);
         }
+
+        public class ComplexPathParameterController : ApiController
+        {
+            [HttpGet]
+            [Route("Filter/{title}")]
+            public string Filter([FromUri]ComplexGetRequest model)
+            {
+                return "foobar = " + model.Title + " => " + model.Bar;
+            }
+
+            public class ComplexGetRequest
+            {
+                [Required]
+                [FromRoute]
+                public string Title { get; set; }
+
+                public string Bar { get; set; }
+            }
+        }
+
+        [TestMethod]
+        public async Task When_complex_parameter_contains_property_with_FromRoute_attribute_then_it_is_generated_as_path_parameter()
+        {
+            // FromRouteAttribute is only available in ASP.NET Core
+            // Issue: https://github.com/NSwag/NSwag/issues/513
+
+            //// Arrange
+            var generator = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings
+            {
+                IsAspNetCore = true, 
+                DefaultPropertyNameHandling = PropertyNameHandling.CamelCase
+            });
+
+            //// Act
+            var document = await generator.GenerateForControllerAsync<ComplexPathParameterController>();
+            var operation = document.Operations.First().Operation;
+            var json = document.ToJson();
+
+            //// Assert
+            Assert.AreEqual(SwaggerParameterKind.Path, operation.Parameters[0].Kind);
+            Assert.AreEqual("title", operation.Parameters[0].Name);
+            Assert.AreEqual(SwaggerParameterKind.Query, operation.Parameters[1].Kind);
+            Assert.AreEqual("bar", operation.Parameters[1].Name);
+        }
+    }
+}
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public class FromRouteAttribute : Attribute
+    {
+        public string Name { get; set; }
     }
 }
