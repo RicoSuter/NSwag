d73383ebe3137a9ac8ae8b936b75fa1fcd7b87fb
egorikas <egorgrishechko@gmail.com>
2017-02-07 00:05:58 +0300

    https://github.com/NSwag/NSwag/issues/521 Start to implement feature. Replacing settings for c# generation to a base settings class

--- src/NSwag.CodeGeneration.CSharp/SwaggerToCSharpGeneratorSettings.cs
+++ src/NSwag.CodeGeneration.CSharp/SwaggerToCSharpGeneratorSettings.cs
@@ -25,6 +25,9 @@ namespace NSwag.CodeGeneration.CSharp
                 NullHandling = NullHandling.Swagger, 
                 TemplateFactory = new DefaultTemplateFactory()
             };
+
+            GenerateResponseClasses = true;
+            ResponseClass = "SwaggerResponse";
         }
 
         /// <summary>Gets or sets the CSharp generator settings.</summary>
@@ -35,5 +38,14 @@ namespace NSwag.CodeGeneration.CSharp
 
         /// <summary>Gets the code generator settings.</summary>
         public override CodeGeneratorSettingsBase CodeGeneratorSettings => CSharpGeneratorSettings;
+
+        /// <summary>Gets or sets a value indicating whether to wrap success responses to allow full response access (experimental).</summary>
+        public bool WrapSuccessResponses { get; set; }
+
+        /// <summary>Gets or sets a value indicating whether to generate the response classes (only needed when WrapSuccessResponses == true, default: true).</summary>
+        public bool GenerateResponseClasses { get; set; }
+
+        /// <summary>Gets or sets the name of the response class (supports the '{controller}' placeholder).</summary>
+        public string ResponseClass { get; set; }
     }
 }
