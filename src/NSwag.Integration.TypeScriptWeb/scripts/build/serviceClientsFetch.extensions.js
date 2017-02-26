c240e3037853057d589c05131588c4f0a03bd54b
Josh Groves <josh@joshgroves.com>
2017-02-25 21:20:48 -0700

    Remove built files

--- src/NSwag.Integration.TypeScriptWeb/scripts/build/serviceClientsFetch.extensions.js
+++
@@ -1,8 +0,0 @@
-const generated = require("serviceClientsFetch");
-class GeoClient extends generated.GeoClientBase {
-    constructor(baseUrl, http) {
-        super(baseUrl, http);
-        //this.jsonParseReviver = (key: string, value: any) => value;
-    }
-}
-//# sourceMappingURL=serviceClientsFetch.extensions.js.map
--- src/NSwag.Integration.TypeScriptWeb/scripts/build/serviceClientsJQueryPromises.extensions.js
+++
@@ -1,7 +0,0 @@
-const generated = require("serviceClientsJQueryPromises");
-class Person extends generated.PersonBase {
-    get fullName() {
-        return this.firstName + " " + this.lastName;
-    }
-}
-//# sourceMappingURL=serviceClientsJQueryPromises.extensions.js.map
