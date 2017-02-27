eed5001225ccbcf4b6c3214f9ebd812914e9b39b
Daniel Gioulakis <dmgdev@gmail.com>
2017-02-27 13:33:12 -0500

    Allows custom parameter binders with type maps.

---
+++ src/NSwag.Annotations/WillReadBodyAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace NSwag.Annotations
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Parameter)]
+    public class WillReadBodyAttribute : Attribute
+    {
+        public WillReadBodyAttribute(bool willReadBody = false)
+        {
+            WillReadBody = willReadBody;
+        }
+
+        public bool WillReadBody { get; }
+    }
+}
