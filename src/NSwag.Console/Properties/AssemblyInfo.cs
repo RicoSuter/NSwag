75200166ed4f795254f58455b6a95bbb9ecb051c
Rico Suter <mail@rsuter.com>
2017-04-19 13:55:36 +0200

    v10.3

--- src/NSwag.Console/Properties/AssemblyInfo.cs
+++ src/NSwag.Console/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwag.Console")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("10.2.*")]
+[assembly: AssemblyVersion("10.3.*")]
