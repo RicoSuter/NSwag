80ce0bf0c0ce600ab8a0f806449f646a066f987a
Rico Suter <mail@rsuter.com>
2017-05-19 18:06:15 +0200

    Improved package tags

--- src/NSwag.AspNetCore/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NSwag.AspNetCore")]
-[assembly: AssemblyDescription("NSwag: The Swagger API toolchain for .NET and TypeScript")]
-[assembly: AssemblyCompany("Rico Suter")]
-[assembly: AssemblyProduct("NSwag.AspNetCore")]
-[assembly: AssemblyCopyright("Copyright © Rico Suter, 2015")]
