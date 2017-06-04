ceda6dbdd32ad5bf3eb42983eff8ce0eeac5edd7
Rico Suter <mail@rsuter.com>
2017-06-04 21:16:47 +0200

    Added sample common project

---
+++ src/NSwag.Sample.Common/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿namespace NSwag.Sample.Common
+{
+    public class WeatherForecast
+    {
+        public string DateFormatted { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+
+        public int TemperatureF
+        {
+            get
+            {
+                return 32 + (int)(TemperatureC / 0.5556);
+            }
+        }
+    }
+}
--- src/NSwag.Sample.NetCoreAngular/Controllers/SampleDataController.cs
+++ src/NSwag.Sample.NetCoreAngular/Controllers/SampleDataController.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NSwag.Sample.Common;
 
 namespace NSwag_Sample_NetCoreAngular.Controllers
 {
@@ -24,21 +24,6 @@ namespace NSwag_Sample_NetCoreAngular.Controllers
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             });
-        }
-
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
         }
     }
 }
