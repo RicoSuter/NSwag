e7a87bb9f3d75ecd21719df947ab18febfd07802
Rico Suter <mail@rsuter.com>
2017-06-04 15:34:15 +0200

    Use .net core 1.1 as fallback, #769

--- src/NSwag.Npm/bin/nswag.js
+++ src/NSwag.Npm/bin/nswag.js
@@ -29,10 +29,13 @@ if (hasFullDotNet && !args.endsWith("--core")) {
     // Run .NET Core version
     var cmd = "dotnet";
     var dotnetVersion = c.exec(cmd, (error, stdout, stderr) => {
-        var dotnetVersion = stdout; 
-        if (dotnetVersion.indexOf("Version  : 1.0.0") !== -1)
-            cmd = 'dotnet "' + __dirname + '/binaries/netcoreapp1.0/dotnet-nswag.dll" ' + args;
-        else if (dotnetVersion.indexOf("Version  : 1.1.0") !== -1)
+        if (!error) {
+            var dotnetVersion = stdout;
+            if (dotnetVersion.indexOf("Version  : 1.0.0") !== -1)
+                cmd = 'dotnet "' + __dirname + '/binaries/netcoreapp1.0/dotnet-nswag.dll" ' + args;
+            else if (dotnetVersion.indexOf("Version  : 1.1.0") !== -1)
+                cmd = 'dotnet "' + __dirname + '/binaries/netcoreapp1.1/dotnet-nswag.dll" ' + args;
+        } else
             cmd = 'dotnet "' + __dirname + '/binaries/netcoreapp1.1/dotnet-nswag.dll" ' + args;
 
         c.execSync(cmd, {stdio:[0,1,2]});	
