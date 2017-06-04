e3128e2b24ddff4d27ec8159b13d7c46f2d688be
Rico Suter <mail@rsuter.com>
2017-06-04 15:40:57 +0200

    Corrected cmd line output

--- src/NSwag.Console/Program.cs
+++ src/NSwag.Console/Program.cs
@@ -8,7 +8,7 @@ namespace NSwag
     {
         static int Main(string[] args)
         {
-            Console.Write("NSwag command line tool for .NET 4.6.2+, ");
+            Console.Write("NSwag command line tool for .NET 4.6+, ");
             var processor = new NSwagCommandProcessor(typeof(NSwagDocument).Assembly, new ConsoleHost());
             return processor.Process(args);
         }
