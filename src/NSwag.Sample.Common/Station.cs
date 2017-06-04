189727514720861bbdd03c241efc7aae22d8991f
Rico Suter <mail@rsuter.com>
2017-06-04 23:25:34 +0200

    Updated njs

--- src/NSwag.Sample.Common/Station.cs
+++ src/NSwag.Sample.Common/Station.cs
@@ -5,5 +5,10 @@
         public string Name { get; set; }
 
         public ExtensionData Data { get; } = new ExtensionData();
+
+        public bool ShouldSerializeName()
+        {
+            return !string.IsNullOrEmpty(Name);
+        }
     }
 }
