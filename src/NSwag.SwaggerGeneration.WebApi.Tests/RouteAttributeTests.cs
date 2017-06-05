3806388850b9ff0980726d713edfe202635f8a8a
Seth Hinze <ultrix@gmail.com>
2017-06-05 15:35:30 -0700

    Follow class hierarchy when locating RouteAttributes
     * Fix InvalidOperationException (Sequence contains more than one
       element) when RouteAttribute is used at different inheritance
       levels.
     * Changed attribute finding code to walk the inheritance hierarchy
       and only look for attributes at each level, stopping once
       an attribute is found.
     * Added unit tests to demonstrate the behavior.

---
+++ src/NSwag.SwaggerGeneration.WebApi.Tests/RouteAttributeTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NSwag.SwaggerGeneration.WebApi.Tests
+{
+    [TestClass]
+    public class RouteAttributeTests
+    {
+       [Route("/api/[controller]")]
+       public abstract class BaseTestController : ApiController
+       {
+       }
+
+        public class FooController : BaseTestController
+        {
+            public void Post()
+            {
+            }
+        }
+
+        [Route("/api/other")]
+        public class BarController : BaseTestController
+        {
+            public void Post()
+            {
+            }
+        }
+
+        [TestMethod]
+        public async Task Use_base_class_route_attribute()
+        {
+            var generator = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+            var document = await generator.GenerateForControllerAsync<FooController>();
+            var swaggerSpecification = document.ToJson();
+
+            StringAssert.Contains(swaggerSpecification, "\"/api/Foo\"");
+        }
+
+        [TestMethod]
+        public async Task Route_attribute_overrides_base()
+        {
+            var generator = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+            var document = await generator.GenerateForControllerAsync<BarController>();
+            var swaggerSpecification = document.ToJson();
+
+            StringAssert.Contains(swaggerSpecification, "\"/api/other\"");
+        }
+    }
+}
