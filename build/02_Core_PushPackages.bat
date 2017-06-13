ecb326d510e40954c4097735b3ab78a4892b8f7e
Rico Suter <mail@rsuter.com>
2017-06-13 18:06:06 +0200

    Fixed npm build

--- build/02_Core_PushPackages.bat
+++
@@ -1,6 +0,0 @@
-set /p apiKey=NuGet API Key: 
-set /p version=Package Version: 
-
-nuget.exe push Packages/NSwag.AspNetCore.%version%.nupkg %apiKey% -s https://nuget.org
-nuget.exe push Packages/NSwag.AssemblyLoaderCore.%version%.nupkg %apiKey% -s https://nuget.org
-nuget.exe push Packages/NSwag.ConsoleCore.%version%.nupkg %apiKey% -s https://nuget.org
