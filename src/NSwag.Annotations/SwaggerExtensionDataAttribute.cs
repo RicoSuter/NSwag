4eab25fb3fb5ba1a45f0b84cc0ec83c5f0aaa12d
Rico Suter <mail@rsuter.com>
2017-06-18 22:59:14 +0200

    Added SwaggerExtensionDataAttribute, #801

---
+++ src/NSwag.Annotations/SwaggerExtensionDataAttribute.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="JsonSchemaExtensionDataAttribute.cs" company="NJsonSchema">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/rsuter/NJsonSchema/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace NSwag.Annotations
+{
+    /// <summary>Adds an extension data property to a class or property.</summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = true)]
+    public class SwaggerExtensionDataAttribute : Attribute
+    {
+        /// <summary>Initializes a new instance of the <see cref="SwaggerExtensionDataAttribute"/> class.</summary>
+        /// <param name="property">The property.</param>
+        /// <param name="value">The value.</param>
+        public SwaggerExtensionDataAttribute(string property, object value)
+        {
+            Property = property;
+            Value = value;
+        }
+
+        /// <summary>Gets the property name.</summary>
+        public string Property { get; private set; }
+
+        /// <summary>Gets the value.</summary>
+        public object Value { get; private set; }
+    }
+}
