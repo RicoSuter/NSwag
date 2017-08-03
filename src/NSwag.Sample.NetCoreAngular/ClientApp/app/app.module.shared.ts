f7469911ef03dc57dcf695ffc7f7a3731a56e94c
Rico Suter <mail@rsuter.com>
2017-08-03 18:43:30 +0200

    Upgraded to ang4.1.2, #

--- src/NSwag.Sample.NetCoreAngular/ClientApp/app/app.module.shared.ts
+++ src/NSwag.Sample.NetCoreAngular/ClientApp/app/app.module.shared.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { UniversalModule } from 'angular2-universal';
+
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+
 import { ServicesModule } from './services.module';
 
-@NgModule({
+export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
@@ -19,7 +20,6 @@ import { ServicesModule } from './services.module';
     ],
     imports: [
         ServicesModule, 
-        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
@@ -28,6 +28,4 @@ import { ServicesModule } from './services.module';
             { path: '**', redirectTo: 'home' }
         ])
     ]
-})
-export class AppModule {
-}
+};
