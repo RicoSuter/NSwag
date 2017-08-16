a1bd8d41d9061e79d1e606fe088d77216561a3da
Rico Suter <mail@rsuter.com>
2017-08-16 22:18:38 +0200

    Improved NPM script

--- src/NSwag.Npm/bin/nswag.js
+++ src/NSwag.Npm/bin/nswag.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 "use strict";
 
-process.title = 'nswag';
+var defaultCoreVersion = "1.1";
+var supportedCoreVersions = ["1.0", "1.1", "2.0"];
 
+// Initialize
+process.title = 'nswag';
 console.log("NSwag NPM CLI");
 var args = process.argv.splice(2, process.argv.length - 2).map(function (a) { return a.indexOf(" ") === -1 ? a : '"' + a + '"' }).join(" ");
 
@@ -21,11 +24,7 @@ if (process.env["windir"]) {
 }
 
 var c = require('child_process');
-if (hasFullDotNet &&
-    args.indexOf("--core") == -1 &&
-    args.indexOf("--core 1.0") == -1 &&
-    args.indexOf("--core 1.1") == -1 &&
-    args.indexOf("--core 2.0") == -1) {
+if (hasFullDotNet && args.indexOf("--core") == -1) {
     // Run full .NET version
     if (args.indexOf("--x86") != -1) {
         var cmd = '"' + __dirname + '/binaries/full/nswag.x86.exe" ' + args;
@@ -36,32 +35,26 @@ if (hasFullDotNet &&
     }
 } else {
     // Run .NET Core version
-    var core10cmd = 'dotnet "' + __dirname + '/binaries/netcoreapp1.0/dotnet-nswag.dll" ' + args;
-    var core11cmd = 'dotnet "' + __dirname + '/binaries/netcoreapp1.1/dotnet-nswag.dll" ' + args;
-    var core20cmd = 'dotnet "' + __dirname + '/binaries/netcoreapp2.0/dotnet-nswag.dll" ' + args;
-    var defaultCmd = core11cmd;
+    var defaultCmd = 'dotnet "' + __dirname + '/binaries/netcoreapp' + defaultCoreVersion + '/dotnet-nswag.dll" ' + args;
+    var infoCmd = "dotnet --version";
+    c.exec(infoCmd, (error, stdout, stderr) => {
+        for (let version of supportedCoreVersions) {
+            var coreCmd = 'dotnet "' + __dirname + '/binaries/netcoreapp' + version + '/dotnet-nswag.dll" ' + args;
 
-    if (args.indexOf("--core 1.0") != -1)
-        c.execSync(core10cmd, { stdio: [0, 1, 2] });
-    else if (args.indexOf("--core 1.1") != -1)
-        c.execSync(core11cmd, { stdio: [0, 1, 2] });
-    else if (args.indexOf("--core 2.0") != -1)
-        c.execSync(core20cmd, { stdio: [0, 1, 2] });
-    else {
-        var cmd = "dotnet --version";
-        c.exec(cmd, (error, stdout, stderr) => {
-            if (!error) {
-                var dotnetVersion = stdout;
-                if (dotnetVersion.indexOf("2.0.0") !== -1)
-                    c.execSync(core20cmd, { stdio: [0, 1, 2] });
-                else if (dotnetVersion.indexOf("1.1.0") !== -1)
-                    c.execSync(core11cmd, { stdio: [0, 1, 2] });
-                else if (dotnetVersion.indexOf("1.0.0") !== -1)
-                    c.execSync(core10cmd, { stdio: [0, 1, 2] });
-                else // default
-                    c.execSync(defaultCmd, { stdio: [0, 1, 2] });
-            } else // default
-                c.execSync(defaultCmd, { stdio: [0, 1, 2] });
-        });
-    }
+            if (args.indexOf("--core " + version) != -1) {
+                c.execSync(coreCmd, { stdio: [0, 1, 2] });
+                return;
+            } else {
+                if (!error) {
+                    var coreVersion = stdout;
+                    if (coreVersion.indexOf(version + ".0") !== -1) {
+                        c.execSync(coreCmd, { stdio: [0, 1, 2] });
+                        return;
+                    }
+                }
+            }
+        }
+        c.execSync(defaultCmd, { stdio: [0, 1, 2] });
+    });
+    return;
 }
