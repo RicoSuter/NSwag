ab2b20d3d3441d4512ff8a6ec4d288174a70d47b
Gozde Ozdemir <gozdeo@interxion.com>
2017-08-24 15:38:19 +0100

    Added CancellationToken to web api controller for abstract controller style and added test methods

--- src/NSwag.CodeGeneration.CSharp.Tests/ControllerGenerationFormatTests.cs
+++ src/NSwag.CodeGeneration.CSharp.Tests/ControllerGenerationFormatTests.cs
@@ -13,7 +13,7 @@ namespace NSwag.CodeGeneration.CSharp.Tests
         public class TestController : ApiController
         {
             [Route("Foo")]
-            public string Foo()
+            public string Foo(string test, bool test2)
             {
                 throw new NotImplementedException();
             }
@@ -86,6 +86,82 @@ namespace NSwag.CodeGeneration.CSharp.Tests
             Assert.IsTrue(code.Contains("private ITestController _implementation;"));
             Assert.IsFalse(code.Contains("abstract class TestController"));
         }
+
+        [TestMethod]
+        public async Task When_partialcontrollermethodwithnorequestparameter_is_async_then_cancellationtoken_is_added()
+        {
+            //// Arrange
+            var swaggerGen = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+            var document = await swaggerGen.GenerateForControllerAsync<TestController>();
+
+            //// Act
+            var codeGen = new SwaggerToCSharpWebApiControllerGenerator(document, new SwaggerToCSharpWebApiControllerGeneratorSettings
+            {
+                ControllerStyle = CSharpControllerStyle.Partial
+            });
+            var code = codeGen.GenerateFile();
+
+            //// Assert
+            Assert.IsTrue(code.Contains("(System.Threading.CancellationToken cancellationToken)"));
+            Assert.IsTrue(code.Contains("_implementation.BarAsync(cancellationToken)"));
+            Assert.IsTrue(code.Contains("System.Threading.Tasks.Task BarAsync(System.Threading.CancellationToken cancellationToken)"));
+        }
+
+        [TestMethod]
+        public async Task When_partialcontrollermethodhasrequestparameter_is_async_then_cancellationtoken_is_added()
+        {
+            //// Arrange
+            var swaggerGen = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+            var document = await swaggerGen.GenerateForControllerAsync<TestController>();
+
+            //// Act
+            var codeGen = new SwaggerToCSharpWebApiControllerGenerator(document, new SwaggerToCSharpWebApiControllerGeneratorSettings
+            {
+            });
+            var code = codeGen.GenerateFile();
+
+            //// Assert
+            Assert.IsTrue(code.Contains("System.Threading.Tasks.Task<string> FooAsync(string test, bool test2, System.Threading.CancellationToken cancellationToken)"));
+            Assert.IsTrue(code.Contains("_implementation.FooAsync(test, test2, cancellationToken);"));
+            Assert.IsTrue(code.Contains("public System.Threading.Tasks.Task<string> Foo(string test, bool test2, System.Threading.CancellationToken cancellationToken)"));
+        }
+
+        [TestMethod]
+        public async Task When_abstractcontrollermethodwithnorequestparameter_is_async_then_cancellationtoken_is_added()
+        {
+            //// Arrange
+            var swaggerGen = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+            var document = await swaggerGen.GenerateForControllerAsync<TestController>();
+
+            //// Act
+            var codeGen = new SwaggerToCSharpWebApiControllerGenerator(document, new SwaggerToCSharpWebApiControllerGeneratorSettings
+            {
+                ControllerStyle = CSharpControllerStyle.Abstract
+            });
+            var code = codeGen.GenerateFile();
+
+            //// Assert
+            Assert.IsTrue(code.Contains("abstract System.Threading.Tasks.Task Bar(System.Threading.CancellationToken cancellationToken)"));
+        }
+
+        [TestMethod]
+        public async Task When_abstractcontrollermethodhasrequestparameter_is_async_then_cancellationtoken_is_added()
+        {
+            //// Arrange
+            var swaggerGen = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+            var document = await swaggerGen.GenerateForControllerAsync<TestController>();
+
+            //// Act
+            var codeGen = new SwaggerToCSharpWebApiControllerGenerator(document, new SwaggerToCSharpWebApiControllerGeneratorSettings
+            {
+                ControllerStyle = CSharpControllerStyle.Abstract
+            });
+            var code = codeGen.GenerateFile();
+
+            //// Assert
+            Assert.IsTrue(code.Contains("System.Threading.Tasks.Task<string> Foo(string test, bool test2, System.Threading.CancellationToken cancellationToken)"));
+        }
+
     }
 }
 
