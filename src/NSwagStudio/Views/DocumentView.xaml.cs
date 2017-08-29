052e95ef7ac22c878c853131d369d4d8f5705845
Rico Suter <mail@rsuter.com>
2017-08-30 00:01:41 +0200

    Revert "Fixed NSwagStudio"
    This reverts commit f8934ab0c69a89a0d481921b4a6727cc60b5aa48.

--- src/NSwagStudio/Views/DocumentView.xaml.cs
+++ src/NSwagStudio/Views/DocumentView.xaml.cs
@@ -71,7 +71,15 @@ namespace NSwagStudio.Views
             get { return (DocumentModel)GetValue(DocumentProperty); }
             set { SetValue(DocumentProperty, value); }
         }
-        
+
+        private void OnGenerate(object sender, RoutedEventArgs e)
+        {
+            App.Telemetry.TrackEvent("Generate", new Dictionary<string, string>
+            {
+                { "Generator", Model.Document.GetSwaggerGeneratorView().Title }
+            });
+        }
+
         private void OnUnloaded(object sender, RoutedEventArgs routedEventArgs)
         {
             foreach (var generatorView in Model.Document.CodeGenerators
