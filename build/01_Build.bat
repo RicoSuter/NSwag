7f2f02829801b0aae5946b1132219c938ece0dc2
Rico Suter <mail@rsuter.com>
2017-08-31 09:15:49 +0200

    Improved build script

--- build/01_Build.bat
+++ build/01_Build.bat
@@ -3,7 +3,7 @@ mkdir "%~dp0\..\src\NSwag.Npm\bin\binaries"
 
 REM Build and copy full .NET command line
 "%~dp0/nuget.exe" restore "%~dp0/../src/NSwag.sln"
-msbuild "%~dp0/../src/NSwag.sln" /p:Configuration=Release /t:rebuild
+msbuild "%~dp0/../src/NSwag.sln" /p:Configuration=Release /t:rebuild || goto :error
 
 xcopy "%~dp0/../src/NSwag.Console/bin/Release/net461" "%~dp0/../src/NSwag.Npm/bin/binaries/full" /E /I /y
 xcopy "%~dp0\..\src\NSwag.Console.x86\bin\Release\net461\NSwag.x86.exe" "%~dp0\..\src\NSwag.Npm\bin\binaries\full"
@@ -11,14 +11,19 @@ xcopy "%~dp0\..\src\NSwag.Console.x86\bin\Release\net461\NSwag.x86.exe.config" "
 
 REM Build and copy .NET Core command line
 dotnet restore "%~dp0/../src/NSwag.ConsoleCore" --no-cache
-dotnet publish "%~dp0/../src/NSwag.ConsoleCore" -c release -f "netcoreapp1.0"
-dotnet publish "%~dp0/../src/NSwag.ConsoleCore" -c release -f "netcoreapp1.1"
-dotnet publish "%~dp0/../src/NSwag.ConsoleCore" -c release -f "netcoreapp2.0"
+dotnet publish "%~dp0/../src/NSwag.ConsoleCore" -c release -f "netcoreapp1.0" -o "bin/release/netcoreapp1.0/Publish" || goto :error
+dotnet publish "%~dp0/../src/NSwag.ConsoleCore" -c release -f "netcoreapp1.1" -o "bin/release/netcoreapp1.1/Publish" || goto :error
+dotnet publish "%~dp0/../src/NSwag.ConsoleCore" -c release -f "netcoreapp2.0" -o "bin/release/netcoreapp2.0/Publish" || goto :error
 
 xcopy "%~dp0/../src/NSwag.ConsoleCore/bin/release/netcoreapp1.0/publish" "%~dp0/../src/NSwag.Npm/bin/binaries/netcoreapp1.0" /E /I /y
 xcopy "%~dp0/../src/NSwag.ConsoleCore/bin/release/netcoreapp1.1/publish" "%~dp0/../src/NSwag.Npm/bin/binaries/netcoreapp1.1" /E /I /y
 xcopy "%~dp0/../src/NSwag.ConsoleCore/bin/release/netcoreapp2.0/publish" "%~dp0/../src/NSwag.Npm/bin/binaries/netcoreapp2.0" /E /I /y
 
 REM Package nuspecs
-"%~dp0/nuget.exe" pack "%~dp0/../src/NSwag.MSBuild/NSwag.MSBuild.nuspec"
-"%~dp0/nuget.exe" pack "%~dp0/../src/NSwagStudio.Chocolatey/NSwagStudio.nuspec"
+"%~dp0/nuget.exe" pack "%~dp0/../src/NSwag.MSBuild/NSwag.MSBuild.nuspec" || goto :error
+"%~dp0/nuget.exe" pack "%~dp0/../src/NSwagStudio.Chocolatey/NSwagStudio.nuspec" || goto :error
+
+goto :EOF
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
