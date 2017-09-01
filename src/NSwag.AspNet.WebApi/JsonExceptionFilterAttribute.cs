a90873b33496774196bb9b9c492a1b70d5f50d4e
Rico Suter <mail@rsuter.com>
2017-09-01 18:26:37 +0200

    Improved JsonExceptionFilterAttribute

--- src/NSwag.AspNet.WebApi/JsonExceptionFilterAttribute.cs
+++ src/NSwag.AspNet.WebApi/JsonExceptionFilterAttribute.cs
@@ -26,36 +26,47 @@ namespace NSwag.AspNet.WebApi
     {
         private readonly bool _hideStackTrace;
         private readonly IDictionary<string, Assembly> _searchedNamespaces;
+        private readonly List<Type> _exceptionTypes = new List<Type>();
 
-        /// <summary> Initializes a new instance of the <see cref="JsonExceptionFilterAttribute"/> class.</summary>
-        public JsonExceptionFilterAttribute()
-            : this(true, new Dictionary<string, Assembly>())
+        /// <summary>Initializes a new instance of the <see cref="JsonExceptionFilterAttribute"/> class.</summary>
+        /// <param name="exceptionTypes">The serialized exception types.</param>
+        public JsonExceptionFilterAttribute(params Type[] exceptionTypes)
+            : this(true, new Dictionary<string, Assembly>(), exceptionTypes)
         {
         }
 
-        /// <summary> Initializes a new instance of the <see cref="JsonExceptionFilterAttribute"/> class.</summary>
+        /// <summary>Initializes a new instance of the <see cref="JsonExceptionFilterAttribute"/> class.</summary>
         /// <param name="hideStackTrace">If set to <c>true</c> the serializer hides stack trace (i.e. sets the StackTrace to 'HIDDEN').</param>
-        public JsonExceptionFilterAttribute(bool hideStackTrace)
-            : this(hideStackTrace, new Dictionary<string, Assembly>())
+        /// <param name="exceptionTypes">The serialized exception types.</param>
+        public JsonExceptionFilterAttribute(bool hideStackTrace, params Type[] exceptionTypes)
+            : this(hideStackTrace, new Dictionary<string, Assembly>(), exceptionTypes)
         {
         }
 
-        /// <summary> Initializes a new instance of the <see cref="JsonExceptionFilterAttribute"/> class.</summary>
+        /// <summary>Initializes a new instance of the <see cref="JsonExceptionFilterAttribute"/> class.</summary>
         /// <param name="hideStackTrace">If set to <c>true</c> the serializer hides stack trace (i.e. sets the StackTrace to 'HIDDEN').</param>
         /// <param name="searchedNamespaces">The namespaces and assemblies to search for exception types.</param>
-        public JsonExceptionFilterAttribute(bool hideStackTrace, IDictionary<string, Assembly> searchedNamespaces)
+        /// <param name="exceptionTypes">The serialized exception types.</param>
+        public JsonExceptionFilterAttribute(bool hideStackTrace, IDictionary<string, Assembly> searchedNamespaces, params Type[] exceptionTypes)
         {
             _hideStackTrace = hideStackTrace;
             _searchedNamespaces = searchedNamespaces;
+            _exceptionTypes.AddRange(exceptionTypes);
         }
 
         /// <summary>Occurs after the action method is invoked.</summary>
         /// <param name="context">The action executed context.</param>
         public override void OnActionExecuted(HttpActionExecutedContext context)
         {
-            if (context.Exception != null)
+            if (context.Exception != null && (_exceptionTypes.Count == 0 || _exceptionTypes.Exists(t => t.IsInstanceOfType(context.Exception))))
             {
-                var json = JsonConvert.SerializeObject(context.Exception, Formatting.None, new JsonExceptionConverter(_hideStackTrace, _searchedNamespaces));
+                var configuration = context.ActionContext.ControllerContext.Configuration;
+
+                var settings = configuration?.Formatters?.JsonFormatter?.SerializerSettings ?? JsonConvert.DefaultSettings?.Invoke();
+                settings = settings != null ? CopySettings(settings) : new JsonSerializerSettings();
+                settings.Converters.Add(new JsonExceptionConverter(_hideStackTrace, _searchedNamespaces));
+
+                var json = JsonConvert.SerializeObject(context.Exception, settings);
                 context.Response = new HttpResponseMessage
                 {
                     StatusCode = (HttpStatusCode)GetStatusCode(context.Exception, context),
@@ -68,33 +79,67 @@ namespace NSwag.AspNet.WebApi
 
         private int GetStatusCode(Exception exception, HttpActionExecutedContext context)
         {
-            var controllerActionDescriptor = context.ActionContext.ActionDescriptor as ReflectedHttpActionDescriptor;
-            if (controllerActionDescriptor != null)
+            if (context.ActionContext.ActionDescriptor is ReflectedHttpActionDescriptor controllerActionDescriptor)
             {
                 var methodInfo = controllerActionDescriptor.MethodInfo;
                 var exceptionType = exception.GetType();
 
 #pragma warning disable 618
-                var responseTypeAttributes = methodInfo.GetCustomAttributes(true).OfType<ResponseTypeAttribute>();
+                var responseTypeAttributes = methodInfo.GetCustomAttributes().OfType<ResponseTypeAttribute>();
 #pragma warning restore 618
-                var responseTypeAttribute = responseTypeAttributes.FirstOrDefault(a => a.ResponseType.IsAssignableFrom(exceptionType.GetTypeInfo()));
+                var responseTypeAttribute = responseTypeAttributes.FirstOrDefault((dynamic a) => exceptionType.IsAssignableFrom((Type)a.ResponseType));
                 if (responseTypeAttribute != null)
                 {
                     var statusCode = 0;
                     if (int.TryParse(responseTypeAttribute.HttpStatusCode, out statusCode))
+                    {
                         return statusCode;
-                }
-
-                var swaggerResponseAttributes = methodInfo.GetCustomAttributes(true).OfType<SwaggerResponseAttribute>();
-                var swaggerResponseAttribute = swaggerResponseAttributes.FirstOrDefault(a => a.Type.IsAssignableFrom(exceptionType.GetTypeInfo()));
-                if (swaggerResponseAttribute != null)
-                {
-                    var statusCode = 0;
-                    if (int.TryParse(swaggerResponseAttribute.StatusCode, out statusCode))
-                        return statusCode;
+                    }
                 }
             }
+
             return 500;
+        }
+
+        private JsonSerializerSettings CopySettings(JsonSerializerSettings settings)
+        {
+            var copy = new JsonSerializerSettings
+            {
+                Context = settings.Context,
+                Culture = settings.Culture,
+                ContractResolver = settings.ContractResolver,
+                ConstructorHandling = settings.ConstructorHandling,
+                CheckAdditionalContent = settings.CheckAdditionalContent,
+                DateFormatHandling = settings.DateFormatHandling,
+                DateFormatString = settings.DateFormatString,
+                DateParseHandling = settings.DateParseHandling,
+                DateTimeZoneHandling = settings.DateTimeZoneHandling,
+                DefaultValueHandling = settings.DefaultValueHandling,
+                EqualityComparer = settings.EqualityComparer,
+                FloatFormatHandling = settings.FloatFormatHandling,
+                Formatting = settings.Formatting,
+                FloatParseHandling = settings.FloatParseHandling,
+                MaxDepth = settings.MaxDepth,
+                MetadataPropertyHandling = settings.MetadataPropertyHandling,
+                MissingMemberHandling = settings.MissingMemberHandling,
+                NullValueHandling = settings.NullValueHandling,
+                ObjectCreationHandling = settings.ObjectCreationHandling,
+                PreserveReferencesHandling = settings.PreserveReferencesHandling,
+                ReferenceResolverProvider = settings.ReferenceResolverProvider,
+                ReferenceLoopHandling = settings.ReferenceLoopHandling,
+                StringEscapeHandling = settings.StringEscapeHandling,
+                TraceWriter = settings.TraceWriter,
+                TypeNameHandling = settings.TypeNameHandling,
+                Binder = settings.Binder,
+                TypeNameAssemblyFormat = settings.TypeNameAssemblyFormat
+            };
+
+            foreach (var converter in settings.Converters)
+            {
+                copy.Converters.Add(converter);
+            }
+
+            return copy;
         }
     }
 }
