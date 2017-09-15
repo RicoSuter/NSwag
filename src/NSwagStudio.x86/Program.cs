6fa18b06b0bf6f83984b562819233d7431031d7b
Rico Suter <mail@rsuter.com>
2017-09-15 17:29:18 +0200

    Updated NJS

--- src/NSwagStudio.x86/Program.cs
+++ src/NSwagStudio.x86/Program.cs
@@ -9,8 +9,26 @@ namespace NSwagStudio.x86
         [STAThread]
         static void Main(string[] args)
         {
+            RegisterAssemblyLoader();
+
             var assembly = Assembly.LoadFile(Path.GetFullPath("NSwagStudio.exe"));
             assembly.GetType("NSwagStudio.App").GetMethod("Main").Invoke(null, new object[0]);
+        }
+
+        private static void RegisterAssemblyLoader()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, eventArgs) =>
+            {
+                var index = eventArgs.Name.IndexOf(",", StringComparison.InvariantCulture);
+                if (index > 0)
+                {
+                    var name = eventArgs.Name.Substring(0, index);
+                    if (File.Exists(name + ".dll"))
+                        return Assembly.LoadFile(Path.GetFullPath(name + ".dll"));
+                }
+
+                return null;
+            };
         }
     }
 }
