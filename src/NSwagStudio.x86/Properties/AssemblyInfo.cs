635b5941e5e116a4752fca714ca9c59f815a83f1
Rico Suter <mail@rsuter.com>
2017-09-18 18:57:41 +0200

    Removed NSwagStudio.x86 (not needed anymore)

--- src/NSwagStudio.x86/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NSwagStudio.x86")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Rico Suter")]
-[assembly: AssemblyProduct("NSwagStudio")]
-[assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("11.7.2")]
