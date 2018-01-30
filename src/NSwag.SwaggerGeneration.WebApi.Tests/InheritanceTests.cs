b7c13b420cee22d543d15d1aa077cd05766ed50a
Rico Suter <mail@rsuter.com>
2018-01-30 20:05:20 +0100

    Fixed inheritance behavior of WebApiToSwaggerGenerator, closes #1138

--- src/NSwag.SwaggerGeneration.WebApi.Tests/InheritanceTests.cs
+++ src/NSwag.SwaggerGeneration.WebApi.Tests/InheritanceTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,8 +10,6 @@ namespace NSwag.SwaggerGeneration.WebApi.Tests
     [TestClass]
     public class InheritanceTests
     {
-
-
         public class TestController : ApiController
         {
             public void Post([FromBody]CC value)
@@ -44,6 +44,43 @@ namespace NSwag.SwaggerGeneration.WebApi.Tests
 
             //// Assert
             Assert.AreEqual(2, Regex.Matches(Regex.Escape(swaggerSpecification), "allOf").Count); // must have an allOf in BB and CC, no more (rest are refs)
+        }
+
+        [RoutePrefix("api/common/standard")]
+        public class StandardController : ApiController
+        {
+            [HttpPost, Route("export")]
+            public void ExportStandard(string criteria) {  }
+
+            [HttpPost, Route("foo")]
+            public void Foo(string criteria) { }
+        }
+
+        [RoutePrefix("api/whatever/specific")]
+        public class SpecificController : StandardController
+        {
+            [HttpPost, Route("export")]
+            public void ExportSpecific(string criteria) {  }
+        }
+
+        [TestMethod]
+        public async Task When_there_are_duplicate_paths_through_inheritance_then_the_base_method_is_ignored()
+        {
+            //// Arrange
+            var generator = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+
+            //// Act
+            var document = await generator.GenerateForControllersAsync(new Type[] { typeof(StandardController), typeof(SpecificController) });
+            var json = document.ToJson();
+
+            //// Assert
+            Assert.AreEqual(4, document.Operations.Count());
+
+            Assert.IsTrue(document.Operations.Any(o => o.Path == "/api/common/standard/export"));
+            Assert.IsTrue(document.Operations.Any(o => o.Path == "/api/common/standard/foo"));
+
+            Assert.IsTrue(document.Operations.Any(o => o.Path == "/api/whatever/specific/export"));
+            Assert.IsTrue(document.Operations.Any(o => o.Path == "/api/whatever/specific/foo"));
         }
     }
 }
