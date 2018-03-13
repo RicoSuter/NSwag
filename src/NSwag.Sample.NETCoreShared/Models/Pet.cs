a7115338621424856f77e655d5a24fad20cd82e4
Ryan Nowak <rynowak@microsoft.com>
2018-03-12 23:03:39 -0700

    Add ASP.NET Core 2.1 sample

---
+++ src/NSwag.Sample.NETCoreShared/Models/Pet.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NSwag.Sample
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+
+        [Range(0, 150)]
+        public int Age { get; set; }
+
+        public Category Category { get; set; }
+
+        public bool HasVaccinations { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        public List<Image> Images { get; set; }
+
+        public List<Tag> Tags { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+    }
+}
