438669ce24daf3cd4ced001432e8e7628978b865
Rico Suter <mail@rsuter.com>
2018-04-04 20:33:45 +0200

    Fixed tests

--- src/NSwag.Core.Tests/Serialization/MediaTypesSerializationTests.cs
+++ src/NSwag.Core.Tests/Serialization/MediaTypesSerializationTests.cs
@@ -6,7 +6,7 @@ namespace NSwag.Core.Tests.Serialization
     public class MediaTypesSerializationTests
     {
         [Fact]
-        public void When_when_response_schema_and_example_is_set_then_it_is_serialized_correctly_in_Swagger()
+        public void When_response_schema_and_example_is_set_then_it_is_serialized_correctly_in_Swagger()
         {
             //// Arrange
             var document = CreateDocument(JsonObjectType.String);
@@ -77,7 +77,7 @@ namespace NSwag.Core.Tests.Serialization
         }
 
         [Fact]
-        public void When_when_response_schema_and_example_is_set_as_file_then_it_is_serialized_correctly_in_Swagger()
+        public void When_response_schema_and_example_is_set_as_file_then_it_is_serialized_correctly_in_Swagger()
         {
             //// Arrange
             var document = CreateDocument(JsonObjectType.File);
@@ -103,11 +103,11 @@ namespace NSwag.Core.Tests.Serialization
       }
     }
   }
-}".Replace("\r", ""), json);
+}".Replace("\r", ""), json.Replace("\r", ""));
         }
 
         [Fact]
-        public void When_when_response_schema_and_example_is_set_as_file_then_it_is_serialized_correctly_in_OpenApi()
+        public void When_response_schema_and_example_is_set_as_file_then_it_is_serialized_correctly_in_OpenApi()
         {
             //// Arrange
             var document = CreateDocument(JsonObjectType.File);
@@ -144,7 +144,7 @@ namespace NSwag.Core.Tests.Serialization
     }
   },
   ""components"": {}
-}".Replace("\r", ""), json);
+}".Replace("\r", ""), json.Replace("\r", ""));
         }
 
 
