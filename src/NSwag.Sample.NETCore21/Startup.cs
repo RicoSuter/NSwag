bb491f56c7d21c64afec3d42cea36812f312900b
Rico Suter <mail@rsuter.com>
2018-08-30 21:38:08 +0200

    Fix startup

--- src/NSwag.Sample.NETCore21/Startup.cs
+++ src/NSwag.Sample.NETCore21/Startup.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NSwag.AspNetCore;
-using NSwag.SwaggerGeneration;
-using NSwag.SwaggerGeneration.AspNetCore;
 
 namespace NSwag.Sample.NETCore21
 {
-    public class MySettingsFactory : SwaggerGeneratorSettingsFactoryBase<AspNetCoreToSwaggerGeneratorSettings, IServiceProvider>
-    {
-        protected override async Task ConfigureAsync(AspNetCoreToSwaggerGeneratorSettings settings, IServiceProvider context)
-        {
-            settings.Title = "Hello from settings factory!";
-        }
-    }
-
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -49,11 +37,7 @@ namespace NSwag.Sample.NETCore21
             app.UseHttpsRedirection();
             app.UseMvc();
 
-            app.UseSwaggerWithApiExplorer(settings => settings.SettingsFactory = new MySettingsFactory());
-            app.UseSwagger(typeof(Startup).Assembly, settings => settings.SwaggerRoute = "/oldswagger.json");
-
             app.UseSwaggerUi3WithApiExplorer();
-            //app.UseSwagger(typeof(Startup).Assembly, settings => settings.SwaggerRoute = "/oldswagger.json");
         }
     }
 }
