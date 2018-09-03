343122c43817f00c96273224d13a6ff7d07ff37f
Rico Suter <mail@rsuter.com>
2018-09-03 22:27:04 +0200

    Improved run

--- samples/run.ps1
+++ samples/run.ps1
@@ -39,9 +39,9 @@ NSwagRun "$samplesPath/WithoutMiddleware/Sample.AspNetCore20" "nswag_project" "N
 # WithoutMiddleware/Sample.AspNetCore21
 # NSwagRun "$samplesPath/WithoutMiddleware/Sample.AspNetCore21" "nswag_assembly" "NetCore21" "Release" true
 NSwagRun "$samplesPath/WithoutMiddleware/Sample.AspNetCore21" "nswag_project" "NetCore21" "Release" false
-NSwagRun "$samplesPath/WithoutMiddleware/Sample.AspNetCore21" "nswag_reflection" "NetCore21" "Release" false
+NSwagRun "$samplesPath/WithoutMiddleware/Sample.AspNetCore21" "nswag_reflection" "NetCore21" "Release" true
  
 # NSwagRun "$samplesPath/WithoutMiddleware/Sample.AspNetCore21" "nswag_assembly" "NetCore21" "Debug" true
 NSwagRun "$samplesPath/WithoutMiddleware/Sample.AspNetCore21" "nswag_project" "NetCore21" "Debug" false
-NSwagRun "$samplesPath/WithoutMiddleware/Sample.AspNetCore21" "nswag_reflection" "NetCore21" "Debug" false
+NSwagRun "$samplesPath/WithoutMiddleware/Sample.AspNetCore21" "nswag_reflection" "NetCore21" "Debug" true
 
