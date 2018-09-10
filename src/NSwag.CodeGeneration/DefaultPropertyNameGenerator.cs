c076123287808a1b7e2a0e11a1a76b30c1bd3f8d
Jesse Sweetland <sweetlandj@gmail.com>
2018-09-10 14:46:15 -0500

    Use IPropertyNameGenerator to determine property names of complex query parameters, #1591

---
+++ src/NSwag.CodeGeneration/DefaultPropertyNameGenerator.cs
@@ -0,0 +1,14 @@
+﻿using NJsonSchema;
+using NJsonSchema.CodeGeneration;
+
+namespace NSwag.CodeGeneration
+{
+    internal class DefaultPropertyNameGenerator : IPropertyNameGenerator
+    {
+        /// <inheritdoc />
+        public string Generate(JsonProperty property)
+        {
+            return ConversionUtilities.ConvertToUpperCamelCase(property.Name, true);
+        }
+    }
+}
---
+++ src/NSwag.CodeGeneration/Models/PropertyModel.cs
@@ -0,0 +1,44 @@
+﻿using NJsonSchema;
+
+namespace NSwag.CodeGeneration.Models
+{
+    /// <summary>
+    /// A model representing the property or field of a structure or class
+    /// </summary>
+    public class PropertyModel
+    {
+        /// <summary>
+        /// The key under which the <see cref="JsonProperty"/> is located in the parent JSON object
+        /// </summary>
+        public string Key { get; }
+
+        /// <summary>
+        /// The <see cref="JsonProperty"/> represented by this <see cref="PropertyModel"/>
+        /// </summary>
+        public JsonProperty JsonProperty { get; }
+
+        /// <summary>
+        /// The generated property name suitable for use in the target language
+        /// </summary>
+        public string PropertyName { get; }
+
+        /// <summary>
+        /// Indicates whether the property is a collection
+        /// </summary>
+        public bool IsCollection => JsonProperty.Type == JsonObjectType.Array;
+
+        /// <inheritdoc />
+        public PropertyModel(string key, JsonProperty jsonProperty, string propertyName)
+        {
+            Key = key;
+            JsonProperty = jsonProperty;
+            PropertyName = propertyName;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return PropertyName;
+        }
+    }
+}
