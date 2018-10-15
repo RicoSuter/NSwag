ab8b453cac0a71a96b0abaed1ec8b367ba43f0da
FabianTe <fabiante@users.noreply.github.com>
2018-10-15 15:59:47 +0200

    TS generator will now use AllowAllSuccessCodes option for operation result type.
    If AllowAllSuccessCodes is set to true, the TS client generator will now set the result type accordingly.

--- src/NSwag.CodeGeneration.TypeScript/Models/TypeScriptOperationModel.cs
+++ src/NSwag.CodeGeneration.TypeScript/Models/TypeScriptOperationModel.cs
@@ -64,12 +64,37 @@ namespace NSwag.CodeGeneration.TypeScript.Models
         {
             get
             {
-                var response = GetSuccessResponse();
-                var isNullable = response?.IsNullable(_settings.CodeGeneratorSettings.SchemaType) == true;
+                string resultType = "";
+                bool isNullable = false;
 
-                var resultType = isNullable && SupportsStrictNullChecks && UnwrappedResultType != "void" && UnwrappedResultType != "null" ?
-                    UnwrappedResultType + " | null" :
-                    UnwrappedResultType;
+                if (!_settings.AllowAllSuccessCodes)
+                {
+                    var response = GetSuccessResponse();
+                    isNullable = response?.IsNullable(_settings.CodeGeneratorSettings.SchemaType) == true;
+
+                    resultType = UnwrappedResultType;
+
+                } else
+                {
+                    foreach (string _responseStatus in _operation.ActualResponses.Keys)
+                    {
+                        SwaggerResponse _response = _operation.ActualResponses[_responseStatus];
+                        if (_responseStatus.StartsWith("2"))
+                        {
+                            var _isNullable = _response.IsNullable(_settings.CodeGeneratorSettings.SchemaType);
+                            if (_isNullable)
+                                isNullable = true;
+                            var _type = _generator.GetTypeName(_response.ActualResponseSchema, _isNullable, "Response");
+                            resultType += (resultType != "" ? " | " : "") + _type;
+                        }
+                        
+                        if (resultType == "")
+                            resultType = "void";
+                    }
+                }
+
+                if (isNullable && SupportsStrictNullChecks && UnwrappedResultType != "void" && UnwrappedResultType != "null")
+                    resultType += " | null";
 
                 if (WrapResponse)
                 {
