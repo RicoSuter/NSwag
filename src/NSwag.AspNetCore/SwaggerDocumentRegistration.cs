a509ae4965d667e06ef56d96120a2314916d5a81
Rico Suter <mail@rsuter.com>
2018-12-06 19:33:15 +0100

    Make SwaggerDocumentRegistration public, closes #1798

--- src/NSwag.AspNetCore/SwaggerDocumentRegistration.cs
+++ src/NSwag.AspNetCore/SwaggerDocumentRegistration.cs
@@ -10,16 +10,22 @@ using NSwag.SwaggerGeneration;
 
 namespace NSwag.AspNetCore
 {
-    internal class SwaggerDocumentRegistration
+    /// <summary>A Swagger/OpenAPI document generator registration.</summary>
+    public class SwaggerDocumentRegistration
     {
+        /// <summary>Initializes a new instance of the <see cref="SwaggerDocumentRegistration"/> class.</summary>
+        /// <param name="documentName">The document name.</param>
+        /// <param name="generator">The document generator.</param>
         public SwaggerDocumentRegistration(string documentName, ISwaggerGenerator generator)
         {
             DocumentName = documentName;
             Generator = generator;
         }
 
+        /// <summary>Gets the document name.</summary>
         public string DocumentName { get; }
 
+        /// <summary>Gets the document generator.</summary>
         public ISwaggerGenerator Generator { get; }
     }
 }
