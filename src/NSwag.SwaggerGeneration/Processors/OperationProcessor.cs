62b81014228e6f72e4f63383c946a8f3255fb099
Rico Suter <mail@rsuter.com>
2018-12-13 19:47:54 +0100

    Add AddOperationFilter method and function based OperationProcessor, closes #1821

---
+++ src/NSwag.SwaggerGeneration/Processors/OperationProcessor.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="OperationProcessor.cs" company="NSwag">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/NSwag/NSwag/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using NSwag.SwaggerGeneration.Processors.Contexts;
+
+namespace NSwag.SwaggerGeneration.Processors
+{
+    /// <summary>A function based operation processor.</summary>
+    public class OperationProcessor : IOperationProcessor
+    {
+        private readonly Func<OperationProcessorContext, Task<bool>> _func;
+
+        /// <summary>Initializes a new instance of the <see cref="OperationProcessor"/> class.</summary>
+        /// <param name="func">The processor function.</param>
+        public OperationProcessor(Func<OperationProcessorContext, Task<bool>> func)
+        {
+            _func = func;
+        }
+
+        /// <summary>Processes the specified method information.</summary>
+        /// <param name="context">The processor context.</param>
+        /// <returns>true if the operation should be added to the Swagger specification.</returns>
+        public async Task<bool> ProcessAsync(OperationProcessorContext context)
+        {
+            return await _func(context);
+        }
+    }
+}
