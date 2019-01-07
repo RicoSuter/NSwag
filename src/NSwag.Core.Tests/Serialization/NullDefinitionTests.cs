88879b7bc808d0a8b6b58723f3e2251339b2b7be
Rico Suter <mail@rsuter.com>
2019-01-07 20:38:55 +0100

    Remove null in definitions, closes #1861

---
+++ src/NSwag.Core.Tests/Serialization/NullDefinitionTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace NSwag.Core.Tests.Serialization
+{
+    public class NullDefinitionTests
+    {
+        [Fact]
+        public async Task When_document_has_response_examples_then_it_is_serialized_in_Swagger()
+        {
+            //// Arrange
+            var json = @"{ ""definitions"": { ""definitions"": null } }";
+
+            //// Act
+            var document = await SwaggerDocument.FromJsonAsync(json);
+
+            //// Assert
+            Assert.True(document.Definitions.Count == 0);
+        }
+    }
+}
