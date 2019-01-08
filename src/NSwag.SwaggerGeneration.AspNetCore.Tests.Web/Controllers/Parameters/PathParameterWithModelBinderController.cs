13558bcc4ce8fab82a61e75c5facfe56d47a366a
Thibaut Le Guilly <leguilly.thibaut@gmail.com>
2019-01-08 20:57:57 +0900

    Path param with model binder attribute (#1872)
    * Correct parameter kind when model binder used on path parameter
    
    * Add braces

---
+++ src/NSwag.SwaggerGeneration.AspNetCore.Tests.Web/Controllers/Parameters/PathParameterWithModelBinderController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace NSwag.SwaggerGeneration.AspNetCore.Tests.Web.Controllers.Parameters
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PathParameterWithModelBinderController : Controller
+    {
+        [HttpGet("{stringModel}")]
+        public ActionResult RequiredRouteParamWithModelBinder([ModelBinder(typeof(StringModelBinder))] string stringModel)
+        {
+            return Ok();
+        }
+
+        public class StringModelBinder : IModelBinder
+        {
+            public Task BindModelAsync(ModelBindingContext bindingContext)
+            {
+                bindingContext.Result =
+                    ModelBindingResult.Success(
+                        bindingContext.ValueProvider.GetValue(
+                            bindingContext.ModelName));
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
