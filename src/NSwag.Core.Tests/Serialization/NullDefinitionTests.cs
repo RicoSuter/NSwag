b101396f1fa34abc8b22a84e61f9f2ddc4a23238
Rico Suter <mail@rsuter.com>
2019-01-08 20:02:13 +0100

    Update NJS

--- src/NSwag.Core.Tests/Serialization/NullDefinitionTests.cs
+++ src/NSwag.Core.Tests/Serialization/NullDefinitionTests.cs
@@ -6,7 +6,7 @@ namespace NSwag.Core.Tests.Serialization
     public class NullDefinitionTests
     {
         [Fact]
-        public async Task When_document_has_response_examples_then_it_is_serialized_in_Swagger()
+        public async Task When_document_has_null_schema_definition_then_it_is_ignored()
         {
             //// Arrange
             var json = @"{ ""definitions"": { ""definitions"": null } }";
