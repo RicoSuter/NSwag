b53a27e30ecc1deff20e64177bdb9f55c0c17678
Rico Suter <mail@rsuter.com>
2019-03-20 20:04:39 +0100

    v12.0.17

--- src/NSwagStudio/Properties/AssemblyInfo.cs
+++ src/NSwagStudio/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NSwagStudio")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("12.0.16")]
+[assembly: AssemblyVersion("12.0.17")]
