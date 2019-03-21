8131dc59e504780eb6e2b037a7f4b69abbfd584a
Rico Suter <mail@rsuter.com>
2019-03-22 00:55:37 +0100

    Clean up OperationConsumesProcessor

--- src/NSwag.SwaggerGeneration.WebApi/Processors/OperationConsumesProcessor.cs
+++ src/NSwag.SwaggerGeneration.WebApi/Processors/OperationConsumesProcessor.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections.Generic;
+﻿//-----------------------------------------------------------------------
+// <copyright file="OperationConsumesProcessor.cs" company="NSwag">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/NSwag/NSwag/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Linq;
-
 using NSwag.SwaggerGeneration.Processors;
 using NSwag.SwaggerGeneration.Processors.Contexts;
 
@@ -21,7 +28,7 @@ namespace NSwag.SwaggerGeneration.WebApi.Processors
                 .GetCustomAttributes()
                 .SingleOrDefault(a => a.GetType().Name == "ConsumesAttribute");
 
-            if(consumesAttribute == null)
+            if (consumesAttribute == null)
             {
                 // If the action method does not have a Consumes Attribute we'll try with its containing class
                 consumesAttribute = context.MethodInfo.DeclaringType
@@ -30,11 +37,9 @@ namespace NSwag.SwaggerGeneration.WebApi.Processors
                     .SingleOrDefault(a => a.GetType().Name == "ConsumesAttribute");
             }
 
-            if(
-                consumesAttribute != null &&
-                consumesAttribute.ContentTypes != null)
+            if (consumesAttribute != null && consumesAttribute.ContentTypes != null)
             {
-                if(context.OperationDescription.Operation.Consumes == null)
+                if (context.OperationDescription.Operation.Consumes == null)
                     context.OperationDescription.Operation.Consumes = new List<string>(consumesAttribute.ContentTypes);
                 else
                     context.OperationDescription.Operation.Consumes.AddRange(consumesAttribute.ContentTypes);
