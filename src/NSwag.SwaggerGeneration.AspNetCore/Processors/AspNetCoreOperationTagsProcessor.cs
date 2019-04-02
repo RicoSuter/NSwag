04b03edd7a7b770c7ba33b36185543612f7f2eb2
softworkz <softworkz@hotmail.com>
2019-04-02 21:26:58 +0200

    Use controller name from ApiExplorer instead of custom logic for AspNetCoreToSwaggerGenerator (#2066)
    * Use controller name from ApiExplorer instead of custom logic for AspNetCoreToSwaggerGenerator
    
    * Remove this prefixes
    
    * Rename to AspNetCoreOperationTagsProcessor

---
+++ src/NSwag.SwaggerGeneration.AspNetCore/Processors/AspNetCoreOperationTagsProcessor.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AspNetCoreOperationTagsProcessor.cs" company="NSwag">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/NSwag/NSwag/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc.Controllers;
+using NSwag.SwaggerGeneration.Processors;
+using NSwag.SwaggerGeneration.Processors.Contexts;
+
+namespace NSwag.SwaggerGeneration.AspNetCore.Processors
+{
+    /// <summary>Processes the SwaggerTagsAttribute on the operation method.</summary>
+    public class AspNetCoreOperationTagsProcessor : OperationTagsProcessor
+    {
+        protected override void AddControllerNameTag(OperationProcessorContext context)
+        {
+            var aspNetCoreContext = (AspNetCoreOperationProcessorContext)context;
+            if (aspNetCoreContext.ApiDescription.ActionDescriptor is ControllerActionDescriptor descriptor)
+            {
+                aspNetCoreContext.OperationDescription.Operation.Tags.Add(descriptor.ControllerName);
+                return;
+            }
+
+            base.AddControllerNameTag(context);
+        }
+    }
+}
