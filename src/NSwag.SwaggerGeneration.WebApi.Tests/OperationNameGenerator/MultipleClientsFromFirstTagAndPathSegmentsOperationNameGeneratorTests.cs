6b03970806155caf74b83de87a285eecce37ecc8
Mike Singleton <msingleton@novetta.com>
2019-04-02 10:16:04 -0400

    Operation generation mode from first tag and path segments (#1973)
    * Adds new IOperationNameGenerator class for generating {controller} using the first tag in each operation and generating the operation name based on the path
    
    * Updates names and comments
    
    * Updates copyright file name
    
    * Update first tag operation name generator to use ConversionUtilities
    
    * Add unit test for tag-based operation name generator

---
+++ src/NSwag.SwaggerGeneration.WebApi.Tests/OperationNameGenerator/MultipleClientsFromFirstTagAndPathSegmentsOperationNameGeneratorTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSwag.Annotations;
+using NSwag.CodeGeneration.OperationNameGenerators;
+using NSwag.CodeGeneration.TypeScript;
+using System.Windows;
+using System.Linq;
+
+namespace NSwag.SwaggerGeneration.WebApi.Tests.OperationNameGenerator
+{
+    /// <summary>
+    /// Test class for MultipleClientsFromFirstTagAndPathSegmentsOperationNameGenerator
+    /// </summary>
+    /// <remarks>
+    /// When endpoints are given OperationIds in the format {ControllerClassName}_{id}, use
+    /// MultipleClientsFromOperationIdOperationNameGenerator to ensure each controller gets its own client.
+    /// However, when the OperationId is in a different format use tags with the
+    /// MultipleClientsFromFirstTagAndPathSegmentsOperationNameGenerator to generate unique clients.
+    /// </remarks>
+    [TestClass]
+    public class MultipleClientsFromFirstTagAndPathSegmentsOperationNameGeneratorTests
+    {
+        [SwaggerTag("PointControllerA")]
+        public class PointControllerA
+        {
+            [SwaggerOperation("PointControllerAGetPoint")]
+            [HttpGet]
+            public Point Get(int id) 
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [SwaggerTag("PointControllerB")]
+        public class PointControllerB
+        {
+            [SwaggerOperation("PointControllerBGetPoint")]
+            [HttpGet]
+            public Point Get(int id)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TestMethod]
+        public async Task When_operations_have_different_tags_they_are_grouped_into_different_clients()
+        {
+            //// Arrange
+            var generator = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+            var document = await generator.GenerateForControllersAsync(new List<Type>() { typeof(PointControllerA), typeof(PointControllerB) });
+            var codeGenerator = new SwaggerToTypeScriptClientGenerator(document, new SwaggerToTypeScriptClientGeneratorSettings
+            {
+                OperationNameGenerator = new MultipleClientsFromFirstTagAndPathSegmentsOperationNameGenerator()
+            });
+
+            //// Act
+            var code = codeGenerator.GenerateFile();
+
+            //// Assert
+            Assert.IsTrue(code.Contains("export class PointControllerAClient"));
+            Assert.IsTrue(code.Contains("export class PointControllerBClient"));
+        }
+
+        [TestMethod]
+        public async Task When_operations_have_no_tags_they_are_grouped_into_one_client()
+        {
+            //// Arrange
+            var generator = new WebApiToSwaggerGenerator(new WebApiToSwaggerGeneratorSettings());
+            var document = await generator.GenerateForControllersAsync(new List<Type>() { typeof(PointControllerA), typeof(PointControllerB) });
+
+            // Remove tags
+            foreach (var path in document.Paths.Values)
+            {
+                foreach (var operation in path.Values)
+                {
+                    operation.Tags.Clear();
+                }
+            }
+            var codeGenerator = new SwaggerToTypeScriptClientGenerator(document, new SwaggerToTypeScriptClientGeneratorSettings
+            {
+                OperationNameGenerator = new MultipleClientsFromFirstTagAndPathSegmentsOperationNameGenerator()
+            });
+
+            //// Act
+            var code = codeGenerator.GenerateFile();
+
+            //// Assert
+            Assert.IsTrue(code.Contains("export class Client"));
+            Assert.IsTrue(!code.Contains("export class PointControllerAClient"));
+            Assert.IsTrue(!code.Contains("export class PointControllerBClient"));
+        }
+    }
+}
