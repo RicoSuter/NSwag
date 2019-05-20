b8482cf1c4411d5aa65404a5ff7b58ac9daf5c81
Rico Suter <mail@rsuter.com>
2019-05-20 19:33:35 +0200

    Fix IsolatedCommandBase

--- src/NSwag.Commands/Commands/IsolatedCommandBase.cs
+++ src/NSwag.Commands/Commands/IsolatedCommandBase.cs
@@ -126,7 +126,7 @@ namespace NSwag.Commands
         {
 #if NET461
             yield return new BindingRedirect("Newtonsoft.Json", typeof(JToken), "30ad4fe6b2a6aeed");
-            yield return new BindingRedirect("Namotion.Reflection", typeof(JsonSchema), "c2f9c3bdfae56102");
+            yield return new BindingRedirect("Namotion.Reflection", typeof(ContextualType), "c2f9c3bdfae56102");
             yield return new BindingRedirect("NJsonSchema", typeof(JsonSchema), "c2f9c3bdfae56102");
             yield return new BindingRedirect("NSwag.Core", typeof(SwaggerDocument), "c2d88086e098d109");
             yield return new BindingRedirect("NSwag.SwaggerGeneration", typeof(SwaggerJsonSchemaGenerator), "c2d88086e098d109");
