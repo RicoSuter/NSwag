f1379e41a7a37942a5551677c2aa50f233b134da
Miroslav Jezik <miroslav.jezik@medtronic.com>
2019-10-11 12:18:20 +0200

    Use http method names to distinguish duplicate operations

--- src/NSwag.Core.Tests/OperationIdTests.cs
+++ src/NSwag.Core.Tests/OperationIdTests.cs
@@ -28,5 +28,40 @@ namespace NSwag.Core.Tests
             //// Assert
             Assert.True(document.Operations.GroupBy(o => o.Operation.OperationId).All(g => g.Count() == 1));
         }
+
+        [Fact]
+        public void When_operation_ids_are_not_unique_then_method_names_are_used()
+        {
+            //// Arrange
+            var document = new OpenApiDocument();
+            document.Paths["path1"] = new OpenApiPathItem
+            {
+                {
+                    OpenApiOperationMethod.Get,
+                    new OpenApiOperation { OperationId = "foo" }
+                },
+                {
+                    OpenApiOperationMethod.Post,
+                    new OpenApiOperation { OperationId = "foo" }
+                }
+            };
+
+            document.Paths["path2"] = new OpenApiPathItem
+            {
+                {
+                    OpenApiOperationMethod.Post,
+                    new OpenApiOperation { OperationId = "foo" }
+                }
+            };
+
+            //// Act
+            document.GenerateOperationIds();
+
+            var actual = document.Operations.ToList();
+            //// Assert
+            Assert.Equal("get-foo", actual[0].Operation.OperationId);
+            Assert.Equal("post-foo", actual[1].Operation.OperationId);
+            Assert.Equal("post-foo2", actual[2].Operation.OperationId);
+        }
     }
 }
