da14329652a4934cc3a263fc601529bb3b4bd2ae
Rico Suter <mail@rsuter.com>
2020-03-20 15:55:11 +0100

    Update NJS

---
+++ src/NSwag.Generation.AspNetCore.Tests.Web/Controllers/XmlDocsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NSwag.Generation.AspNetCore.Tests.Web.Controllers
+{
+    [ApiController]
+    [Route("api/xmldocs")]
+    public class XmlDocsController : Controller
+    {
+        [HttpGet]
+        public MyResponse GetResponse()
+        {
+            return new MyResponse();
+        }
+    }
+
+    /// <summary>
+    /// My response.
+    /// </summary>
+    public class MyResponse
+    {
+        /// <summary>
+        /// My property.
+        /// </summary>
+        public string MyProperty { get; set; }
+    }
+}
