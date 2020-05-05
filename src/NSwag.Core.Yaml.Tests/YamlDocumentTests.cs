e48eec1de0f4d668f3a0c3c3d1a58b2c728cf545
alex-bolenok-centralreach <56651099+alex-bolenok-centralreach@users.noreply.github.com>
2020-05-05 09:59:19 -0400

    Fixes #2820 (#2821)
    * Reproduces #2820
    
    * Fixes #2820
    
    * Revert auto-formatting

--- src/NSwag.Core.Yaml.Tests/YamlDocumentTests.cs
+++ src/NSwag.Core.Yaml.Tests/YamlDocumentTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace NSwag.Core.Yaml.Tests
@@ -55,6 +56,34 @@ paths:
             Assert.NotNull(document);
             Assert.Equal("bar", document.Paths.First().Value.ExtensionData["x-swagger-router-controller"]);
             Assert.Contains("x-swagger-router-controller: bar", yaml);
+        }
+
+        [Fact]
+        public async Task When_yaml_with_custom_property_which_is_an_object_is_loaded_then_document_is_not_null()
+        {
+          //// Arrange
+          var yaml = @"swagger: '2.0'
+info:
+  title: foo
+  version: '1.0'
+paths:
+  /bar:
+    x-swagger-router-controller:
+     bar: baz
+    get:
+      responses:
+        '200':
+          description: baz";
+
+          //// Act
+          var document = await OpenApiYamlDocument.FromYamlAsync(yaml);
+          yaml = document.ToYaml();
+
+          //// Assert
+          Assert.NotNull(document);
+          Assert.Equal(JObject.Parse(@"{""bar"": ""baz""}"), document.Paths.First().Value.ExtensionData["x-swagger-router-controller"]);
+          Assert.Equal("baz", document.Paths.First().Value["get"].Responses["200"].Description);
+          Assert.Contains("bar: baz", yaml);
         }
     }
 }
