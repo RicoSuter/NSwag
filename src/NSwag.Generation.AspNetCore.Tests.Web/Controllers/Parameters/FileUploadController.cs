4cd515ddfa5f22366cfdc29aae9da22688018799
Rico Suter <mail@rsuter.com>
2020-05-12 18:57:13 +0200

    Improve form data support (OpenAPI 3), closes #2491

---
+++ src/NSwag.Generation.AspNetCore.Tests.Web/Controllers/Parameters/FileUploadController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NJsonSchema.Annotations;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace NSwag.Generation.AspNetCore.Tests.Web.Controllers.Parameters
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileUploadController : Controller
+    {
+        [HttpPost]
+        public ActionResult UploadFile([NotNull] IFormFile file, [FromForm, NotNull]string test)
+        {
+            return Ok();
+        }
+
+        [HttpPost("UploadAttachment")]
+        public async Task<IActionResult> UploadAttachment(
+            [FromRoute][Required] string caseId,
+            [FromForm] CaseAttachmentModel model)
+        {
+            return Ok();
+        }
+
+        public class CaseAttachmentModel
+        {
+            public string Description { get; set; }
+
+            public IFormFile Contents { get; set; }
+        }
+    }
+}
---
+++ src/NSwag.Generation.AspNetCore.Tests/Parameters/FileUploadTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NJsonSchema;
+using NSwag.Generation.AspNetCore.Tests.Web.Controllers.Parameters;
+using Xunit;
+
+namespace NSwag.Generation.AspNetCore.Tests.Parameters
+{
+    public class FileUploadTests : AspNetCoreTestsBase
+    {
+        [Fact]
+        public async Task WhenOperationHasFormDataFile_ThenItIsInRequestBody()
+        {
+            // Arrange
+            var settings = new AspNetCoreOpenApiDocumentGeneratorSettings { SchemaType = SchemaType.OpenApi3 };
+
+            // Act
+            var document = await GenerateDocumentAsync(settings, typeof(FileUploadController));
+            var json = document.ToJson();
+
+            // Assert
+            var operation = document.Operations.First(o => o.Operation.OperationId == "FileUpload_UploadFile").Operation;
+            var schema = operation.RequestBody.Content["multipart/form-data"].Schema;
+
+            Assert.Equal("binary", schema.Properties["file"].Format);
+            Assert.Equal(JsonObjectType.String, schema.Properties["file"].Type);
+
+            Assert.Equal(JsonObjectType.String, schema.Properties["test"].Type);
+        }
+
+        [Fact]
+        public async Task WhenOperationHasFormDataComplex_ThenItIsInRequestBody()
+        {
+            // Arrange
+            var settings = new AspNetCoreOpenApiDocumentGeneratorSettings { SchemaType = SchemaType.OpenApi3 };
+
+            // Act
+            var document = await GenerateDocumentAsync(settings, typeof(FileUploadController));
+            var json = document.ToJson();
+
+            // Assert
+            var operation = document.Operations.First(o => o.Operation.OperationId == "FileUpload_UploadAttachment").Operation;
+
+            Assert.NotNull(operation);
+            Assert.Contains(@"""requestBody"": {
+          ""content"": {
+            ""multipart/form-data"": {
+              ""schema"": {
+                ""properties"": {
+                  ""Description"": {
+                    ""type"": ""string"",
+                    ""nullable"": true
+                  },
+                  ""Contents"": {
+                    ""type"": ""string"",
+                    ""format"": ""binary"",
+                    ""nullable"": true
+                  }
+                }
+              }
+            }
+          }
+        },", json);
+        }
+    }
+}
