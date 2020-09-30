276acdc2b708c8ece726231c3f6cde9b2f95862f
luk355 <luk355@gmail.com>
2020-10-01 00:46:57 +0200

    feat: add unit tests for external references

--- src/NSwag.Core.Tests/Serialization/PathItemTests.cs
+++ src/NSwag.Core.Tests/Serialization/PathItemTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace NSwag.Core.Tests.Serialization
@@ -6,11 +7,34 @@ namespace NSwag.Core.Tests.Serialization
     public class PathItemTests
     {
         [Fact]
-        public async Task Foo()
+        public async Task When_file_contains_path_reference_to_another_file_it_is_loaded()
         {
-            //var json = @""; // TODO
-            var document = await OpenApiDocument.FromFileAsync("TestFiles/pathItemTests.json");
+            var document = await OpenApiDocument.FromFileAsync("TestFiles/path-reference.json");
 
+            Assert.NotNull(document);
+        }
+
+        [Fact]
+        public async Task When_file_contains_schema_reference_to_another_file_it_is_loaded()
+        {
+            var document = await OpenApiDocument.FromFileAsync("TestFiles/schema-reference.json");
+
+            Assert.NotNull(document);
+            Assert.Equal("Referenced test object.", document.Paths.First().Value.Values.First().Responses.First().Value.Content.First().Value.Schema.Description);
+        }
+
+        [Fact]
+        public async Task When_file_contains_response_reference_to_another_file_it_is_loaded()
+        {
+            var document = await OpenApiDocument.FromFileAsync("TestFiles/response-reference.json");
+
+            Assert.NotNull(document);
+        }
+
+        [Fact]
+        public async Task When_file_contains_parameter_reference_to_another_file_it_is_loaded()
+        {
+            var document = await OpenApiDocument.FromFileAsync("TestFiles/parameter-reference.json");
 
             Assert.NotNull(document);
         }
