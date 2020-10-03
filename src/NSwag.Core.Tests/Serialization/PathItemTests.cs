55b6e15e398c09e7c087bb5fc1f58b1667f7a540
davidmbillie <davidmbillie@gmail.com>
2020-10-02 23:28:39 -0400

    Clean up PathItemTest

--- src/NSwag.Core.Tests/Serialization/PathItemTests.cs
+++ src/NSwag.Core.Tests/Serialization/PathItemTests.cs
@@ -2,8 +2,6 @@
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
-using NJsonSchema;
-using NJsonSchema.Generation;
 using Xunit;
 
 namespace NSwag.Core.Tests.Serialization
@@ -15,30 +13,15 @@ namespace NSwag.Core.Tests.Serialization
         [Fact]
         public async Task PathItem_With_External_Ref_Can_Be_Serialized()
         {
-            string refPath = GetTestDirectory() + "/Serialization/PathItemTest/refs/PathItem.json";
-            JsonSchema refPathItem = await JsonSchema.FromFileAsync(refPath);
-
             string path = GetTestDirectory() + "/Serialization/PathItemTest/PathItemWithRef.json";
 
-            OpenApiDocument result = await OpenApiDocument.FromFileAsync(path,
-					  schema4 =>
-					  {
-						  JsonSchemaResolver schemaResolver = new JsonSchemaResolver(
-							 schema4,
-							 new JsonSchemaGeneratorSettings());
+            OpenApiDocument doc = await OpenApiDocument.FromFileAsync(path);
 
-						  var resolver = new JsonReferenceResolver(schemaResolver);
-						  // someone referencing "definitions.schema.json? Use this content instead
-						  resolver.AddDocumentReference(refPath, refPathItem);
-
-						  return resolver;
-					  });
-
-            var resultPaths = result.Paths;
-            var resultPathItem = resultPaths[aValidPath];
-            var actualPathItem = resultPathItem.ActualPathItem;
-            var resultOperation = actualPathItem["get"];
-            Assert.True(resultOperation.ActualResponses.ContainsKey("200"));
+            var paths = doc.Paths;
+            var pathItem = paths[aValidPath];
+            var actualPathItem = pathItem.ActualPathItem;
+            var getOperation = actualPathItem["get"];
+            Assert.True(getOperation.ActualResponses.ContainsKey("200"));
         }
 
         private string GetTestDirectory()
