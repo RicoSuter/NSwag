3d482b2041d052f85399c94d5d52b49be9ff771e
Felipe Prata L Borges <felipe@prabor.com.br>
2021-01-15 13:01:22 +0000

    Fixing FormData generated for typescript should add the content array property in a foreach statement to handle correctly in the api. (#3250)
    Co-authored-by: Felipe Borges <Felipe.Borges@fenergo.com>

--- src/NSwag.CodeGeneration.TypeScript.Tests/ArrayParameterTests.cs
+++ src/NSwag.CodeGeneration.TypeScript.Tests/ArrayParameterTests.cs
@@ -68,5 +68,80 @@ namespace NSwag.CodeGeneration.TypeScript.Tests
             //// Assert
             Assert.Contains(@"elementId.forEach(item => { url_ += ""elementId="" + encodeURIComponent("""" + item) + ""&""; });", code);
         }
+
+
+        [Fact]
+        public async Task when_content_is_formdata_with_property_array_then_content_should_be_added_in_foreach_in_typescript()
+        {
+            var json = @"{
+              ""x-generator"": ""NSwag v13.5.0.0 (NJsonSchema v10.1.15.0 (Newtonsoft.Json v11.0.0.0))"",
+              ""openapi"": ""3.0.0"",
+              ""info"": {
+                ""title"": ""My Title"",
+                ""version"": ""1.0.0""
+              },
+              ""paths"": {
+                ""/api/FileUpload/UploadFile"": {
+                  ""post"": {
+                    ""tags"": [
+                      ""FileUpload""
+                    ],
+                    ""operationId"": ""FileUpload_UploadFile"",
+                    ""requestBody"": {
+                      ""content"": {
+                        ""multipart/form-data"": {
+                          ""schema"": {
+                            ""properties"": {
+                              ""file"": {
+                                ""type"": ""string"",
+                                ""format"": ""binary""
+                              },
+                              ""arrayOfIds"": {
+                                ""uniqueItems"": true,
+                                ""type"": ""array"",
+                                ""items"": {
+                                  ""type"": ""string""
+                                },
+                                ""description"": ""Hash Set of of strings in the DTO request"",
+                                ""nullable"": true
+                              },
+                              ""test"": {
+                                ""type"": ""string""
+                              }
+                            }
+                          }
+                        }
+                      }
+                    },
+                    ""responses"": {
+                      ""200"": {
+                        ""description"": """",
+                        ""content"": {
+                          ""application/octet-stream"": {
+                            ""schema"": {
+                              ""type"": ""string"",
+                              ""format"": ""binary""
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                },
+    
+              },
+              ""components"": {}
+            }";
+
+            var document = await OpenApiDocument.FromJsonAsync(json);
+
+            //// Act
+            var codeGenerator = new TypeScriptClientGenerator(document, new TypeScriptClientGeneratorSettings());
+            var code = codeGenerator.GenerateFile();
+
+            //// Assert
+            Assert.Contains("const content_ = new FormData();", code);
+            Assert.Contains("arrayOfIds.forEach(item_ => content_.append(\"arrayOfIds\", item_.toString());", code);
+        }
     }
 }
