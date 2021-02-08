4e0646427cc36880d8a606937d24fa62b537fe98
Felipe Prata L Borges <felipe.prabor@gmail.com>
2021-02-08 13:41:40 +0000

    Adding support to the object parameter for multipart/form-data (#3283)
    * Fixing FormData generated for typescript should add the content array property in a foreach statement to handle correctly in the api.
    
    * Quick fix for a ')' missing in the liquid template typescript
    
    * Adding support to the object parameter for multipart/form-data. It needs to add the content as json stringify
    
    * Adding support to Array and Object parameters when the content is multipart/form-data in csharp client
    
    * Update Client.Class.liquid
    
    * Update Client.Class.liquid
    
    Co-authored-by: Felipe Borges <Felipe.Borges@fenergo.com>
    Co-authored-by: Rico Suter <mail@rsuter.com>

--- src/NSwag.CodeGeneration.CSharp.Tests/ArrayParameterTests.cs
+++ src/NSwag.CodeGeneration.CSharp.Tests/ArrayParameterTests.cs
@@ -68,5 +68,81 @@ namespace NSwag.CodeGeneration.CSharp.Tests
             //// Assert
             Assert.Contains(@"foreach (var item_ in elementId) { urlBuilder_.Append(System.Uri.EscapeDataString(""elementId"") + ""="").Append(System.Uri.EscapeDataString(ConvertToString(item_, System.Globalization.CultureInfo.InvariantCulture))).Append(""&""); }", code);
         }
+        
+        [Fact]
+        public async Task when_content_is_formdata_with_property_array_then_content_should_be_added_in_foreach_in_csharp()
+        {
+            var json = @"{
+              ""x-generator"": ""NSwag v13.5.0.0 (NJsonSchema v10.1.15.0 (Newtonsoft.Json v11.0.0.0))"",
+              ""openapi"": ""3.0.0"",
+              ""info"": {
+                ""title"": ""My Title"",
+                ""version"": ""1.0.0""
+              },
+              ""paths"": {
+                ""/api/FileUpload/UploadFile"": {
+                  ""post"": {
+                    ""tags"": [
+                      ""FileUpload""
+                    ],
+                    ""operationId"": ""FileUpload_UploadFile"",
+                    ""requestBody"": {
+                      ""content"": {
+                        ""multipart/form-data"": {
+                          ""schema"": {
+                            ""properties"": {
+                              ""file"": {
+                                ""type"": ""string"",
+                                ""format"": ""binary""
+                              },
+                              ""arrayOfIds"": {
+                                ""uniqueItems"": true,
+                                ""type"": ""array"",
+                                ""items"": {
+                                  ""type"": ""string""
+                                },
+                                ""description"": ""Hash Set of of strings in the DTO request"",
+                                ""nullable"": true
+                              },
+                              ""test"": {
+                                ""type"": ""string""
+                              }
+                            }
+                          }
+                        }
+                      }
+                    },
+                    ""responses"": {
+                      ""200"": {
+                        ""description"": """",
+                        ""content"": {
+                          ""application/octet-stream"": {
+                            ""schema"": {
+                              ""type"": ""string"",
+                              ""format"": ""binary""
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                },
+    
+              },
+              ""components"": {}
+            }";
+
+            var document = await OpenApiDocument.FromJsonAsync(json);
+
+            //// Act
+            var settings = new CSharpClientGeneratorSettings { ClassName = "MyClass" };
+            var generator = new CSharpClientGenerator(document, settings);
+            var code = generator.GenerateFile();
+
+            //// Assert
+            Assert.Contains("var content_ = new System.Net.Http.MultipartFormDataContent(boundary_);", code);
+            Assert.Contains("foreach (var item_ in arrayOfIds)", code);
+            Assert.Contains("content_.Add(new System.Net.Http.StringContent(item_), \"arrayOfIds\");", code);
+        }
     }
 }
---
+++ src/NSwag.CodeGeneration.TypeScript.Tests/ObjectParameterTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace NSwag.CodeGeneration.TypeScript.Tests
+{
+    public class ObjectParameterTests
+    {
+        [Fact]
+        public async Task when_content_is_formdata_with_property_object_then_content_should_be_json_typescript()
+        {
+            var json = @"{
+              ""x-generator"": ""NSwag v13.5.0.0 (NJsonSchema v10.1.15.0 (Newtonsoft.Json v11.0.0.0))"",
+              ""openapi"": ""3.0.0"",
+              ""info"": {
+                ""title"": ""My Title"",
+                ""version"": ""1.0.0""
+              },
+              ""paths"": {
+                ""/api/FileUpload/UploadFile"": {
+                  ""post"": {
+                    ""tags"": [
+                      ""FileUpload""
+                    ],
+                    ""operationId"": ""FileUpload_UploadFile"",
+                    ""requestBody"": {
+                      ""content"": {
+                        ""multipart/form-data"": {
+                          ""schema"": {
+                            ""properties"": {
+                              ""file"": {
+                                ""type"": ""string"",
+                                ""format"": ""binary""
+                              },
+                              ""propertyDto"": {
+                                ""type"": ""object"",
+                                ""description"": ""Configurable properties""
+                              },
+                              ""test"": {
+                                ""type"": ""string""
+                              }
+                            }
+                          }
+                        }
+                      }
+                    },
+                    ""responses"": {
+                      ""200"": {
+                        ""description"": """",
+                        ""content"": {
+                          ""application/octet-stream"": {
+                            ""schema"": {
+                              ""type"": ""string"",
+                              ""format"": ""binary""
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                },
+    
+              },
+              ""components"": {}
+            }";
+
+            var document = await OpenApiDocument.FromJsonAsync(json);
+
+            //// Act
+            var codeGenerator = new TypeScriptClientGenerator(document, new TypeScriptClientGeneratorSettings());
+            var code = codeGenerator.GenerateFile();
+
+            //// Assert
+            Assert.Contains("const content_ = new FormData();", code);
+            Assert.Contains("content_.append(\"propertyDto\", JSON.stringify(propertyDto))", code);
+        }
+    }
+}
