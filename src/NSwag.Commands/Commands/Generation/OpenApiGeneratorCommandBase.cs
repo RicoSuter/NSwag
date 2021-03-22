1bf1e881481c5c5873761d66b9280381bd6af156
Jens H. Eriksen <26311915+Jens-H-Eriksen@users.noreply.github.com>
2021-03-22 16:26:13 +0100

    Update OpenApiGeneratorCommandBase.cs

--- src/NSwag.Commands/Commands/Generation/OpenApiGeneratorCommandBase.cs
+++ src/NSwag.Commands/Commands/Generation/OpenApiGeneratorCommandBase.cs
@@ -282,39 +282,38 @@ namespace NSwag.Commands.Generation
             }
             else
             {
-                    // Search Program class and use CreateWebHostBuilder method
-                    var assemblies = LoadAssemblies(AssemblyPaths, assemblyLoader);
-                    var firstAssembly = assemblies.FirstOrDefault() ?? throw new InvalidOperationException("No assembly are be loaded from AssemblyPaths.");
+                // Search Program class and use CreateWebHostBuilder method
+                var assemblies = LoadAssemblies(AssemblyPaths, assemblyLoader);
+                var firstAssembly = assemblies.FirstOrDefault() ?? throw new InvalidOperationException("No assembly are be loaded from AssemblyPaths.");
 
-                    var programType = firstAssembly.ExportedTypes.First(t => t.Name == "Program") ??
-                        throw new InvalidOperationException("The Program class could not be determined in the assembly '" + firstAssembly.FullName + "'.");
+                var programType = firstAssembly.ExportedTypes.First(t => t.Name == "Program") ??
+                    throw new InvalidOperationException("The Program class could not be determined in the assembly '" + firstAssembly.FullName + "'.");
 
-                    var method =
-                        programType.GetRuntimeMethod("CreateWebHostBuilder", new[] { typeof(string[]) }) ??
-                        programType.GetRuntimeMethod("CreateWebHostBuilder", new Type[0]);
+                var method =
+                    programType.GetRuntimeMethod("CreateWebHostBuilder", new[] { typeof(string[]) }) ??
+                    programType.GetRuntimeMethod("CreateWebHostBuilder", new Type[0]);
+
+                if (method != null)
+                {
+                    return ((IWebHostBuilder)method.Invoke(
+                        null, method.GetParameters().Length > 0 ? new object[] { new string[0] } : new object[0])).Build();
+                }
+                else
+                {
+#if NET5_0 || NETCOREAPP3_0
+                    method =
+                        programType.GetRuntimeMethod("CreateHostBuilder", new[] { typeof(string[]) }) ??
+                        programType.GetRuntimeMethod("CreateHostBuilder", new Type[0]);
 
                     if (method != null)
                     {
-                        return ((IWebHostBuilder)method.Invoke(
+                        return ((Microsoft.Extensions.Hosting.IHostBuilder)method.Invoke(
                             null, method.GetParameters().Length > 0 ? new object[] { new string[0] } : new object[0])).Build();
                     }
                     else
-                    {
-#if NET5_0 || NETCOREAPP3_0
-                        method =
-                            programType.GetRuntimeMethod("CreateHostBuilder", new[] { typeof(string[]) }) ??
-                            programType.GetRuntimeMethod("CreateHostBuilder", new Type[0]);
-
-                        if (method != null)
-                        {
-                            return ((Microsoft.Extensions.Hosting.IHostBuilder)method.Invoke(
-                                null, method.GetParameters().Length > 0 ? new object[] { new string[0] } : new object[0])).Build();
-                        }
-                        else
 #endif
-                        {
-                            throw new InvalidOperationException("The Program class '" + programType.FullName + "' does not have a CreateWebHostBuilder()/CreateHostBuilder() method.");
-                        }
+                    {
+                        throw new InvalidOperationException("The Program class '" + programType.FullName + "' does not have a CreateWebHostBuilder()/CreateHostBuilder() method.");
                     }
                 }
             }
