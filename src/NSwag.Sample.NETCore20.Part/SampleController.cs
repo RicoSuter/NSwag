bde619459c942eda70f61d2496c3ce438c61a087
Rico Suter <mail@rsuter.com>
2021-05-18 11:15:04 +0200

    Add sample project again, closes #3470

---
+++ src/NSwag.Sample.NETCore20.Part/SampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NSwag.Sample.NETCore20.Part
+{
+    [Route("/sample")]
+    public class SampleController : Controller
+    {
+        [HttpPost]
+        public string GetSample()
+        {
+            return null;
+        }
+    }
+}
