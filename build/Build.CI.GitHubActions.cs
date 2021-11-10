1fae10c891667bd7085317237da0af1f436e0ca4
Marko Lahma <marko.lahma@gmail.com>
2021-11-10 17:17:52 +0200

    Add support for installing .NET 5 SDK to NUKE build

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.CI.GitHubActions.Configuration;
+using Nuke.Common.Execution;
+using Nuke.Common.Utilities;
 
-[GitHubActions(
+[CustomGitHubActionsAttribute(
     "pr",
     GitHubActionsImage.WindowsLatest,
     // GitHubActionsImage.UbuntuLatest,
@@ -10,9 +14,9 @@ using Nuke.Common.CI.GitHubActions;
     //OnPullRequestBranches = new[] { DevelopBranch },
     PublishArtifacts = false,
     InvokedTargets = new[] { nameof(InstallDependencies), nameof(Compile), nameof(Test), nameof(Pack) },
-    CacheKeyFiles = new[] { "global.json", "src/**/*.csproj", "src/**/package.json" })
+    CacheKeyFiles = new[] { "global.json", "src/**/*.csproj", "src/**/package.json" }),
 ]
-[GitHubActions(
+[CustomGitHubActionsAttribute(
     "build",
     GitHubActionsImage.WindowsLatest,
     // GitHubActionsImage.UbuntuLatest,
@@ -26,4 +30,47 @@ using Nuke.Common.CI.GitHubActions;
 ]
 public partial class Build
 {
+}
+
+class CustomGitHubActionsAttribute : GitHubActionsAttribute
+{
+    public CustomGitHubActionsAttribute(string name, GitHubActionsImage image, params GitHubActionsImage[] images) : base(name, image, images)
+    {
+    }
+
+    protected override GitHubActionsJob GetJobs(GitHubActionsImage image, IReadOnlyCollection<ExecutableTarget> relevantTargets)
+    {
+        var job = base.GetJobs(image, relevantTargets);
+
+        var newSteps = new List<GitHubActionsStep>(job.Steps);
+        foreach (var version in new[] { "6.0.*", "5.0.*", "3.1.*", "2.1.*" })
+        {
+            newSteps.Insert(1, new GitHubActionsSetupDotNetStep
+            {
+                Version = version
+            });
+        }
+
+        job.Steps = newSteps.ToArray();
+        return job;
+    }
+}
+
+class GitHubActionsSetupDotNetStep : GitHubActionsStep
+{
+    public string Version { get; init; }
+
+    public override void Write(CustomFileWriter writer)
+    {
+        writer.WriteLine("- uses: actions/setup-dotnet@v1");
+
+        using (writer.Indent())
+        {
+            writer.WriteLine("with:");
+            using (writer.Indent())
+            {
+                writer.WriteLine($"dotnet-version: {Version}");
+            }
+        }
+    }
 }
