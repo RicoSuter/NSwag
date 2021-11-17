5a510d1660093498880c6f48b11517990b4fa117
Marko Lahma <marko.lahma@gmail.com>
2021-11-17 19:41:36 +0200

    Modernize and cleanup csproj files

--- src/NSwagStudio/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NSwagStudio")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Rico Suter")]
-[assembly: AssemblyProduct("NSwagStudio")]
-[assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("13.14.4")]
