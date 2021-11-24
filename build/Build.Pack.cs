545b79ff4d3fafab0dcc5c538b786ecc474bd3e6
Marko Lahma <marko.lahma@gmail.com>
2021-11-24 20:55:01 +0200

    pack should follow all testing and samples

--- build/Build.Pack.cs
+++ build/Build.Pack.cs
@@ -19,6 +19,7 @@ public partial class Build
     // logic from 01_Build.bat
     Target Pack => _ => _
         .DependsOn(Compile)
+        .After(Test, IntegrationTest, UnitTest, Samples)
         .Produces(ArtifactsDirectory / "*.*")
         .Executes(() =>
         {
