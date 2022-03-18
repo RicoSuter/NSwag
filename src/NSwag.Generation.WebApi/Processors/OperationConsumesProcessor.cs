7da177555451f440957b84f3b61f49c726c4943b
pavpavferg <37802963+pavpavferg@users.noreply.github.com>
2022-03-18 10:15:09 -0400

    Used TryAddConsumes method to avoid duplicates in 'Consumes'
    Previous implementation either re-created a list of 'content types or blindly appended a value from ConsumesAttribute attribute.
    That blind adding caused content types duplication (in case if other preceding processes have already added a similar content type).
    The fix uses a built-in TryAddConsumes method which makes sure that the content type is unique.

--- src/NSwag.Generation.WebApi/Processors/OperationConsumesProcessor.cs
+++ src/NSwag.Generation.WebApi/Processors/OperationConsumesProcessor.cs
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // <copyright file="OperationConsumesProcessor.cs" company="NSwag">
 //     Copyright (c) Rico Suter. All rights reserved.
 // </copyright>
@@ -39,14 +39,8 @@ namespace NSwag.Generation.WebApi.Processors
 
             if (consumesAttribute != null && consumesAttribute.ContentTypes != null)
             {
-                if (context.OperationDescription.Operation.Consumes == null)
-                {
-                    context.OperationDescription.Operation.Consumes = new List<string>(consumesAttribute.ContentTypes);
-                }
-                else
-                {
-                    context.OperationDescription.Operation.Consumes.AddRange(consumesAttribute.ContentTypes);
-                }
+                foreach (var contentType in consumesAttribute.ContentTypes)
+                    context.OperationDescription.Operation.TryAddConsumes(contentType);
             }
 
             return true;
