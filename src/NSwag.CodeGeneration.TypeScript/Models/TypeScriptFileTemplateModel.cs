4749251c712d90ff906251e23f8044031993c934
tomvdk <tvandekerkhof@hotmail.com>
2022-05-18 19:19:07 +0200

    Added check on SupportsTypeOnlyImports when TypeScript version is >= 4.3 and updated File.liquid template for TypeScript (#4005)
    * - The TypeScript compiler type checking produces an error for the import of the axios types
    - Not possible to change the preserveValueImports or isolatedModules compiler options because they are needed by vite-vue
    - also see Issue 3964 - type-only imports for axios (https://github.com/RicoSuter/NSwag/issues/3964)
    - Changed axios imports to support import type when typescript version >= 4.3 by adding SupportsTypeOnlyImports in TypeScriptFileTemplateModel.cs
    - It is supported as from TypeScript version 3.8 but only versions 2.7, 4.3, so picked 4.3 (https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html)
    
    * changed to TypeScript 3.8 as its supported from this version (https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html)
    You can't set it in the UI but user can define free value in JSON configuration.
    
    Co-authored-by: Tom van de Kerkhof <tom.vandekerkhof2@kbc.be>

--- src/NSwag.CodeGeneration.TypeScript/Models/TypeScriptFileTemplateModel.cs
+++ src/NSwag.CodeGeneration.TypeScript/Models/TypeScriptFileTemplateModel.cs
@@ -164,6 +164,9 @@ namespace NSwag.CodeGeneration.TypeScript.Models
         /// <summary>Gets a value indicating whether the target TypeScript version supports override keyword.</summary>
         public bool SupportsOverrideKeyword => _settings.TypeScriptGeneratorSettings.SupportsOverrideKeyword;
 
+        /// <summary>Gets a value indicating whether the target TypeScript version supports Type-Only imports</summary>
+        public bool SupportsTypeOnlyImports => _settings.TypeScriptGeneratorSettings.TypeScriptVersion >= 3.8m;
+
         private string GenerateExtensionCodeAfter()
         {
             var clientClassesVariable = "{" + string.Join(", ", _clientTypes
