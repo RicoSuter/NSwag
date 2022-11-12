927435f2bc69712254aff0da6a8102589c609264
geckogeckogecko <lorna_harvey@hotmail.com>
2022-11-12 17:38:16 +0000

    Began fixing error where multi-file generation does not work.

--- src/NSwag.CodeGeneration.CSharp/CSharpClientGenerator.cs
+++ src/NSwag.CodeGeneration.CSharp/CSharpClientGenerator.cs
@@ -69,6 +69,16 @@ namespace NSwag.CodeGeneration.CSharp
             }
         }
 
+		/// <summary>
+		/// Calculate the filename for the code artifact, for use in multi-file generation .
+		/// </summary>
+		/// <param name="artifact">The artifact for which a filename is required.</param>
+		/// <returns>The name of the file into which to place the generated code.</returns>
+		protected override string GetOutputFileName(CodeArtifact artifact)
+        {
+            return artifact.TypeName + ".cs";
+        }
+
         /// <summary>Creates an operation model.</summary>
         /// <param name="operation">The operation.</param>
         /// <param name="settings">The settings.</param>
