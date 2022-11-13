ce893c3a33e3b316bc7ae74a966cafb2e582d876
geckogeckogecko <lorna_harvey@hotmail.com>
2022-11-13 15:16:30 +0000

    Continued process of making GenerateFiles work in NSwagStudio

---
+++ src/NSwagStudio/Converters/StyleTypeToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NSwagStudio.Converters
+{
+	public class StyleTypeToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value.Equals(parameter))
+			{
+				return Visibility.Visible;
+			}
+			return Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
