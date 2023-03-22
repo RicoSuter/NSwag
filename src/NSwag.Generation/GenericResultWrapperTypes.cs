cd8b9b7ab3df54e2faaf6cfd1d4ab3c1b99a8c56
Alasdair Cunningham-Smith <alasdair@acs-solutions.co.uk>
2023-03-22 13:44:42 +0000

    lahma suggestion for performance

--- src/NSwag.Generation/GenericResultWrapperTypes.cs
+++ src/NSwag.Generation/GenericResultWrapperTypes.cs
@@ -3,27 +3,23 @@ using System.Linq;
 
 namespace NSwag.Generation
 {
+    internal static class GenericResultWrapperTypes
+    {
+        internal static bool IsGenericWrapperType( string typeName )
+            =>
+                typeName == "Task`1" ||
+                typeName == "ValueTask`1" ||
+                typeName == "JsonResult`1" ||
+                typeName == "ActionResult`1"
+            ;
 
-	internal static class GenericResultWrapperTypes
-	{
-		private readonly static String[] WrapperTypeNames =
-		{
-			"Task`1",
-			"ValueTask`1",
-			"JsonResult`1",
-			"ActionResult`1"
-		};
-
-		internal static bool IsGenericWrapperType(string typeName)
-			=> WrapperTypeNames.Contains( typeName );
-
-		internal static void RemoveGenericWrapperTypes<T>(ref T o,Func<T,string> getName,Func<T,T> unwrap)
-		{
-			// We iterate because a common signature is public async Task<ActionResult<T>> Action()
-			while (IsGenericWrapperType(getName(o)))
-			{
-				o = unwrap(o);
-			}
-		}
-	}
+        internal static void RemoveGenericWrapperTypes<T>(ref T o, Func<T,string> getName, Func<T,T> unwrap)
+        {
+            // We iterate because a common signature is public async Task<ActionResult<T>> Action()
+            while (IsGenericWrapperType(getName(o)))
+            {
+                o = unwrap(o);
+            }
+        }
+    }
 }
