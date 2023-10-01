3b2caca252317f769e6b473d87c28aabfd3b3e1e
Marko Lahma <marko.lahma@gmail.com>
2023-10-01 23:24:07 +0300

    Cleaned project dependencies (#4530)

--- src/NSwag.Console/Program.cs
+++ src/NSwag.Console/Program.cs
@@ -9,7 +9,7 @@ namespace NSwag
     {
         private static Task<int> Main(string[] args)
         {
-            Console.Write("NSwag command line tool for .NET 4.6.1+ " + RuntimeUtilities.CurrentRuntime + ", ");
+            Console.Write("NSwag command line tool for .NET 4.6.2+ " + RuntimeUtilities.CurrentRuntime + ", ");
             var processor = new NSwagCommandProcessor(new ConsoleHost());
             return processor.ProcessAsync(args);
         }
