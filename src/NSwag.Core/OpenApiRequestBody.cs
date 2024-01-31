9211c03d672c1e3e2117a956f31457f95c739146
Elzo Lubbers <elzo.lubbers@stater.nl>
2024-01-31 11:23:24 +0100

    Support $ref in request-bodies

--- src/NSwag.Core/OpenApiRequestBody.cs
+++ src/NSwag.Core/OpenApiRequestBody.cs
@@ -112,5 +112,18 @@ namespace NSwag
         object IJsonReference.PossibleRoot => ParentOperation?.Parent?.Parent;
 
         #endregion
+
+        public override OpenApiRequestBody Reference
+        {
+            get
+            {
+                return base.Reference;
+            }
+            set
+            {
+                base.Reference = value;
+                (Parent as OpenApiOperation)?.UpdateBodyParameter();
+            }
+        }
     }
 }
