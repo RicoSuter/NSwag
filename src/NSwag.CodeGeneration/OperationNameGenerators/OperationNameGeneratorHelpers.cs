c1222f2019aaeb641cdc079592ccde0562e32769
joriswit <git@joriswit.nl>
2025-03-19 21:54:58 +0100

    #2447 Improved code with suggestions in lahma's review.

--- src/NSwag.CodeGeneration/OperationNameGenerators/OperationNameGeneratorHelpers.cs
+++ src/NSwag.CodeGeneration/OperationNameGenerators/OperationNameGeneratorHelpers.cs
@@ -13,8 +13,11 @@ namespace NSwag.CodeGeneration.OperationNameGenerators
 {
     static class OperationNameGeneratorHelpers
     {
+        private static readonly UnicodeCategory[] s_validCategoriesFirstChar = [UnicodeCategory.UppercaseLetter, UnicodeCategory.LowercaseLetter, UnicodeCategory.TitlecaseLetter, UnicodeCategory.ModifierLetter, UnicodeCategory.OtherLetter, UnicodeCategory.LetterNumber];
+        private static readonly UnicodeCategory[] s_additionalValidCategoriesOtherChars = [UnicodeCategory.DecimalDigitNumber, UnicodeCategory.ConnectorPunctuation, UnicodeCategory.NonSpacingMark, UnicodeCategory.SpacingCombiningMark];
+
         /// <summary>
-        /// Modifies an identifier, so it will be both be a valid CSharp identfier as a valid TypeScript identifier
+        /// Modifies an identifier, so it will be both be a valid CSharp identifier as a valid TypeScript identifier
         /// </summary>
         /// <remarks>
         /// Because this function is used for both C# as TypeScript, some valid identifiers will be changed because they are invalid in the other. E.g. $test is valid TypeScript, but not C#.
@@ -25,14 +28,11 @@ namespace NSwag.CodeGeneration.OperationNameGenerators
         {
             if (identifier.Length > 0)
             {
-                var validCategoriesFirstChar = new[] { UnicodeCategory.UppercaseLetter, UnicodeCategory.LowercaseLetter, UnicodeCategory.TitlecaseLetter, UnicodeCategory.ModifierLetter, UnicodeCategory.OtherLetter, UnicodeCategory.LetterNumber };
-                var additionalValidCategoriesOtherChars = new[] { UnicodeCategory.DecimalDigitNumber, UnicodeCategory.ConnectorPunctuation, UnicodeCategory.NonSpacingMark, UnicodeCategory.SpacingCombiningMark };
-
                 var newIdent = new StringBuilder();
                 var firstChar = identifier[0];
                 var firstCat = char.GetUnicodeCategory(firstChar);
-                if (firstChar != '_' && !validCategoriesFirstChar.Contains(firstCat)
-                    && additionalValidCategoriesOtherChars.Contains(firstCat))
+                if (firstChar != '_' && Array.IndexOf(s_validCategoriesFirstChar, firstCat) == -1
+                    && Array.IndexOf(s_additionalValidCategoriesOtherChars, firstCat) != -1)
                 {
                     // Add underscore if first char is invalid as a first char, but valid as an additional char
                     newIdent.Append('_');
@@ -42,7 +42,7 @@ namespace NSwag.CodeGeneration.OperationNameGenerators
                 {
                     // Replace all characters that are invalid with underscores
                     var cat = char.GetUnicodeCategory(character);
-                    if (validCategoriesFirstChar.Contains(cat) || additionalValidCategoriesOtherChars.Contains(cat))
+                    if (Array.IndexOf(s_validCategoriesFirstChar, cat) != -1 || Array.IndexOf(s_additionalValidCategoriesOtherChars, cat) != -1)
                     {
                         newIdent.Append(character);
                     }
