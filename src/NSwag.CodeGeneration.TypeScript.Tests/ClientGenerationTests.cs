6905a31b650aa4227e44758d5c4a838e70be5e3a
Marko Lahma <marko.lahma@gmail.com>
2025-04-20 13:10:18 +0300

    Tolerate missing schema in OperationModelBase.GetActualParameters (#5147)
    * Tolerate missing schema in GetActualParameters
    * add verification tests for C# and TypeScript output
    * cleanup and optimize

---
+++ src/NSwag.CodeGeneration.TypeScript.Tests/ClientGenerationTests.cs
@@ -0,0 +1,32 @@
+﻿using NJsonSchema;
+
+namespace NSwag.CodeGeneration.TypeScript.Tests;
+
+public class ClientGenerationTests
+{
+    [Fact]
+    public async Task CanGenerateFromJiraOpenApiSpecification()
+    {
+        await VerifyOutput("JIRA_OpenAPI", "jira-open-api.json");
+    }
+
+    private static async Task VerifyOutput(string name, string fileName)
+    {
+        Environment.SetEnvironmentVariable("NSWAG_NOVERSION", "true");
+
+        var specification = await File.ReadAllTextAsync(Path.Combine("TestData", fileName));
+
+        var document = await OpenApiDocument.FromJsonAsync(specification, "", SchemaType.OpenApi3);
+        var generator = new TypeScriptClientGenerator(document, new TypeScriptClientGeneratorSettings
+        {
+            GenerateOptionalParameters = true,
+        });
+
+        var code = generator.GenerateFile();
+
+        await Verify(code)
+            .UseFileName(name)
+            .ScrubLinesContaining(StringComparison.OrdinalIgnoreCase, "Generated using the NSwag toolchain")
+            .UseDirectory("Snapshots");;
+    }
+}
