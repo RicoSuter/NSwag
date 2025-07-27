9e570292bb9a838777b3305776949359f8ecfe3e
Nikita Zimin <nzeemin@users.noreply.github.com>
2025-07-27 10:24:26 +0300

    Fix: C# Code Generation generates method with return default(void) (#4826)
    ---------
    
    Co-authored-by: Nikita Zimin <n.zimin@tinkoff.ru>
    Co-authored-by: Marko Lahma <marko.lahma@gmail.com>

--- src/NSwag.CodeGeneration.CSharp/Models/CSharpOperationModel.cs
+++ src/NSwag.CodeGeneration.CSharp/Models/CSharpOperationModel.cs
@@ -115,7 +115,9 @@ namespace NSwag.CodeGeneration.CSharp.Models
         /// <summary>
         /// The default value of the result type, i.e. default(T) or default(T)! depending on whether NRT are enabled.
         /// </summary>
-        public string UnwrappedResultDefaultValue => $"default({UnwrappedResultType}){(_settings is CSharpClientGeneratorSettings { CSharpGeneratorSettings.GenerateNullableReferenceTypes: true } ? "!" : "")}";
+        public string UnwrappedResultDefaultValue => HasResult
+            ? $"default({UnwrappedResultType}){(_settings is CSharpClientGeneratorSettings { CSharpGeneratorSettings.GenerateNullableReferenceTypes: true } ? "!" : "")}"
+            : null;
 
         /// <summary>Gets or sets the synchronous type of the result.</summary>
         public string SyncResultType
